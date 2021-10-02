7b04225e46accb5f28445db1810a6dd0d219948e
김민수 <minsu980107@naver.com>
2021-10-02 23:58:10 +0900

    이진탐색
    이진탐색

--- PythonAlgo/이것이 취업을위한 파이썬/PART4부록/PART4.py
+++ PythonAlgo/이것이 취업을위한 파이썬/PART4부록/PART4.py
@@ -122,6 +122,36 @@ def heapsort(iterable):
 result = heapsort([1,3,5,7,9,2,4,6,8,0])
 print(result)
 
+#bisect 라이브러리
+
+print("bisect 이진탐색 라이브러리")
+
+from bisect import bisect_left, bisect_right
+
+a=[1,2,4,4,8]
+x=4
+
+print(bisect_left(a,x))
+print(bisect_right(a,x))
+
+# 정렬된 리스트에서 값이 특정 범위에 속하는 원소의 개수를 구하고자 할 때 효과적
+
+from bisect import bisect_left, bisect_right
+
+# 값이 [left_value, right_value]인 데이터의 개수를 반환하는 함수
+def count_by_range(a, left_value, right_value):
+    right_index = bisect_right(a, right_value)
+    left_index = bisect_left(a,left_value)
+    return right_index - left_index
+
+#리스트 선언
+a= [1,2,3,3,3,3,4,4,8,9]
+
+# 값이 4인 데이터 개수 출력
+print(count_by_range(a,4,4))
+
+#값이 [-1, 3] 범위에 있는 데이터 개수 출력
+print(count_by_range(a,-1,3))
 
 
 
