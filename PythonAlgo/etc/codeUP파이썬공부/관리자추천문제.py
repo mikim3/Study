c225e8abbd0c928efec61cbdcef5a2f9ffa71956
김민수 <minsu980107@naver.com>
2021-10-19 23:48:11 +0900

    Update 관리자추천문제.py

--- PythonAlgo/etc/codeUP파이썬공부/관리자추천문제.py
+++ PythonAlgo/etc/codeUP파이썬공부/관리자추천문제.py
@@ -1,5 +1,17 @@
 # 3002번문제 기억력테스트
 
+from bisect import bisect_left,bisect_right  # 이진탐색 라이브러리
+
+def count_by_range(a,left_value,right_value):   #범위안에 값이 몇개 있나 탐색  
+    right_index = bisect_right(a,right_value)
+    left_index=bisect_left(a,left_value)
+    return right_index - left_index
+def count_by_range(a,left_value,right_value):
+    right_index = bisect_right(a,right_value)
+    left_index=bisect_left(a,left_value)
+    return right_index - left_index
+
+
 import sys
 n=int(input())  # 
 a=list(map(int,sys.stdin.readline().rstrip().split()))
