d1b9b3a4eb28f98b77812ac8b9bafe1b44a84ab7
김민수 <minsu980107@naver.com>
2021-12-24 20:00:02 +0900

    문자열 완료
    문자열 완료

--- PythonAlgo/백준알고리즘풀기/단계별풀어보기.py
+++ PythonAlgo/백준알고리즘풀기/단계별풀어보기.py
@@ -480,6 +480,48 @@
 #     count+=2 # 끝에 두개 안샌거 더하기
 # print(count)
 
+# 1316 번 문제 
+# 그룹 단어 체커
+# 문자열 다루는 기술이 부족해서 문법찾아봄
+
+# 이 문법 꼭 잘쓰기 if in 
+# if word[j] in arr:   # word[j] 가 arr안에 있다면 
+
+n = int(input())   # 받을 문자열 갯수
+count=0  # 그룹 단어 갯수 
+arr=[]  # 
+
+for i in range(n):   # n번 반복하여 받기
+    now='1'  # 현재 그룹되는 문자
+    word=input()  # 문자열 받기 현재 분석할 단어  
+    #  조건 1 직전 인덱스에 문자와 같다면 넘어간다
+    #  조건 2 직전 인덱스에 문자와 다르다면 지금까지 쭉 나온 인덱스와 비교해본다 만약 같은게 있다면 count하지말고 이번 단어를 넘긴다.
+    #  만약 같은게 없다면 다시 다음 문자로 넘어간다.
+    for j in range(len(word)):
+        if  now==word[j]:
+            arr.append(word[j])
+            pass
+        else:
+            if word[j] in arr:   # word[j] 가 arr안에 있다면 
+                count-=1
+                break
+        arr.append(word[j]) # 
+        now=word[j]
+    count+=1 
+    arr.clear()  # 
+print(count)
+
+
+####### 쓸모있는 문법 기억하기 
+# word = input()
+# arr = ['a','b','c']
+# for z in word:
+#     print(z)
+#     if z in arr:
+#         print("겹쳐")
+
+
+
 ######## 8단계 기본수학1 
 
 # 1712번 문제
@@ -490,7 +532,7 @@
 # sum=0
 # count=0
 
-# if d<= 0:  # 진짜로 손해보는장사
+# if d <= 0:  # 진짜로 손해보는장사
 #     print(-1)
 # else:
 #     print(a//d+1)
