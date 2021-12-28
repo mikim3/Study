cfe26ea1ffc837505c9a3f4b047f15ba08df6bd5
김민수 <minsu980107@naver.com>
2021-12-28 21:12:57 +0900

    인기문제집 풀이중
    인기문제집 풀이중

---
+++ PythonAlgo/백준알고리즘풀기/백준etc/인기문제집_Python배우기(1~50).py
@@ -0,0 +1,33 @@
+
+
+# 1001 번문제
+# A-B
+
+# a,b = list(map(int,input().split()))
+# print(a-b)
+
+# 1008
+# A/B
+
+
+# 10430
+# 나머지
+
+# 2558
+# A+B -2
+
+
+# 3046번 문제
+# R2
+
+# r1,s=list(map(int,input().split()))
+# # (R1 + R2)/2 == s
+# # 2s-R1 =R2
+# print(s*2-r1)
+
+# 2163번 문제
+# 초콜릿 자르기
+
+# n,m=list(map(int,input().split()))
+# print((n-1)*(m-1))
+
