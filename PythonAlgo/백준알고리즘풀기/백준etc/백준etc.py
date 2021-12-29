f22e05d238f6d73a0d2df1988e7f1756add708c4
김민수 <minsu980107@naver.com>
2021-12-29 22:41:26 +0900

    별찍기 풀기
    별찍기 풀기

---
+++ PythonAlgo/백준알고리즘풀기/백준etc/백준etc.py
@@ -0,0 +1,25 @@
+
+# 12865 번 문제
+# 평범한 배낭 
+
+# n,k = list(map(int,input().split()))  #  물품수 n   버틸수 있는 무게 k
+#
+
+
+# 2446번 문제
+# 별 찍기 - 9 
+
+# n=int(input())  # 
+# for i in range(n,0,-1):
+#     print(" "*(n-i),end="")
+#     print("*"*(2*i-1))
+# for i in range(1,n):
+#     print(" "*(n-i-1),end="")
+#     print("*"*(2*i+1))
+    
+
+# 2448 번 문제
+# 별 찍기 - 11
+
+# 
+
