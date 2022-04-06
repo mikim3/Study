5e5fcb68f08ea3df96a15ff2bfd0bddc092d5952
김민수 <minsu980107@naver.com>
2022-04-06 19:35:10 +0900

    알고리즘

--- PythonAlgo/백준알고리즘풀기/백준알고리즘기초1/알고리즘기초1.py
+++ PythonAlgo/백준알고리즘풀기/백준알고리즘기초1/알고리즘기초1.py
@@ -3,32 +3,29 @@
 # 파이썬은 따로 스택구조를 제공안한다고 한다.
 # 대신 list.pop() 이 있으니 쉽게 구현가능하다.
 
-import sys
-input = sys.stdin.readline
+# import sys
+# input = sys.stdin.readline
 
-n = int(input())
-stack = []
-for i in range(n):
-    command = input().split()
-    if (command[0] == 'push'):
-        stack.append(command[1])    
-    if (command[0] == 'pop'):
-        if len(stack) == 0:
-            print(-1)
-        else:
-            print(stack.pop())
-    if (command[0] == 'size'):
-        print(len(stack))
-    if (command[0] == 'empty'):
-        if(len(stack) == 0):
-            print(1)
-        else:
-            print(0)    
-    if (command[0] == 'top'):
-        if(len(stack) == 0):
-            print(-1)
-        else:
-            print(stack[-1])
-
-
-# 9093번 단어뒤집기
+# n = int(input())
+# stack = []
+# for i in range(n):
+#     command = input().split()
+#     if (command[0] == 'push'):
+#         stack.append(command[1])    
+#     if (command[0] == 'pop'):
+#         if len(stack) == 0:
+#             print(-1)
+#         else:
+#             print(stack.pop())
+#     if (command[0] == 'size'):
+#         print(len(stack))
+#     if (command[0] == 'empty'):
+#         if(len(stack) == 0):
+#             print(1)
+#         else:
+#             print(0)    
+#     if (command[0] == 'top'):
+#         if(len(stack) == 0):
+#             print(-1)
+#         else:
+#             print(stack[-1])
