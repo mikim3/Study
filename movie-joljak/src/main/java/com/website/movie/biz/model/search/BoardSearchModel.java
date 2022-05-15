ff8765c4393aa515453addae68f74975df54a57c
김민수 <minsu980107@naver.com>
2022-05-15 23:37:19 +0900

    졸작실험용 폴더 사용

---
+++ movie-joljak/src/main/java/com/website/movie/biz/model/search/BoardSearchModel.java
@@ -0,0 +1,12 @@
+package com.website.movie.biz.model.search;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+public class BoardSearchModel {
+}
--- movie-joljak/src/main/java/com/website/movie/web/controller/UserController.java
+++ movie-joljak/src/main/java/com/website/movie/web/controller/UserController.java
@@ -3,12 +3,13 @@ package com.website.movie.web.controller;
 import com.website.movie.biz.dto.UserDto;
 import com.website.movie.biz.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 public class UserController {
 
@@ -18,4 +19,31 @@ public class UserController {
     public List<UserDto> gets() {
         return userService.gets();
     }
+
+    @GetMapping("/user")
+    public @ResponseBody String user() {
+        return "login";
+    }
+
+    @GetMapping("/manager")
+    public @ResponseBody String manager() {
+        return "manager";
+    }
+
+    //스프링 시큐리티가 해당주소는 낚아채버린다. - SecurityConfig 파일 생성 후 작동안함
+    @GetMapping("/login")
+    public  String login() {
+        return "loginForm";
+    }
+
+    @GetMapping("/join")
+    public @ResponseBody String join() {
+        return "join";
+    }
+
+    @GetMapping("/joinProc")
+    public @ResponseBody String joinProc() {
+        return "회원가입 완료됨!";
+    }
+
 }
