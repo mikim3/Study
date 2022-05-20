f8a322af558295ab65be3362fbd737e2b937c37c
김민수 <minsu980107@naver.com>
2022-05-20 23:56:58 +0900

    web Study

---
+++ SchoolStudy/webProjectclass/dev-stu-week11-v1.1.0/nodejs-ex-stu/01template_ex/01ejs/controllers/homeController-stu.js
@@ -0,0 +1,12 @@
+"use strict";
+
+exports.sendReqParam = (req, res) => {
+  let veg = req.params.vegetable;
+  res.send(`This is the page for ${veg}`);
+};
+
+
+
+exports.respondWithName = (req, res) => {
+  res.render("index");
+};
---
+++ SchoolStudy/webProjectclass/dev-stu-week11-v1.1.0/nodejs-ex-stu/01template_ex/01ejs/controllers/homeController.js
@@ -0,0 +1,15 @@
+"use strict";
+
+exports.sendReqParam = (req, res) => {
+  let veg = req.params.vegetable;
+  res.send(`This is the page for ${veg}`);
+};
+
+exports.respondWithName = (req, res) => {
+  let paramsName = req.params.myName;
+  res.render("index", { name: paramsName });
+};
+
+// exports.respondWithName = (req, res) => {
+//   res.render("index");
+// };
