83ba20cd07b5f3ee7b8d1ae9d682b7ddd5e811c5
김민수 <minsu980107@naver.com>
2022-06-03 23:57:28 +0900

    정리

--- movie-project05161617/src/main/java/com/website/movie/biz/service/EmailService.java
+++
@@ -1,7 +0,0 @@
-package com.website.movie.biz.service;
-
-import javax.mail.MessagingException;
-
-public interface EmailService {
-    void sendMail(String toEmail, String subject, String message) throws MessagingException;
-}
