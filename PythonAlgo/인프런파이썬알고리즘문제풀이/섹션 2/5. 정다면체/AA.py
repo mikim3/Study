67753c0c1db9e212cecd837e22c3d6297fe1f0ff
김민수 <minsu980107@naver.com>
2022-07-05 14:08:51 +0900

    정다면체문제

---
+++ PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 2/5. 정다면체/AA.py
@@ -0,0 +1,36 @@
+# collections.defaultdict(int)는 서칭해서 기억해냄 
+# 35분걸림
+
+# 딕셔너리로 내가 품
+
+# import collections
+# n,m = map(int,input().split())
+# dic = collections.defaultdict(int)
+
+# for i in range(1,n+1):
+#     for j in range(1,m+1):
+#         dic[i+j] += 1 
+# li_max = [k for k,v in dic.items() if max(dic.values()) == v]  # 리스트 컴프리헨션으로 딕셔너리 최대값 출력
+
+# for i in li_max:
+#     print(i,end=' ')
+
+# 리스트로 해보기
+
+n,m = map(int,input().split())
+li = [0]* (n+m+3)
+
+for i in range(1,n+1):
+    for j in range(1,m+1):
+        li[i+j] += 1 
+max = 0
+
+for i in range(len(li)):
+    if li[i] > max:
+        max = li[i] 
+for i in range(len(li)):
+    if li[i] == max:
+        print(i,end=' ')
+
+
+
