32c07fd3dfb104bf744a4aa856b66d8e0add6de5
김민수 <minsu980107@naver.com>
2022-07-10 22:41:13 +0900

    뒤집은 소수문제

--- PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 2/8. 뒤집은 소수/AA.py
+++ PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 2/8. 뒤집은 소수/AA.py
@@ -1,28 +1,32 @@
 # 14 17
 
-
-
 def reverse(x):
-    
-    
-    return 
+    x_str = str(x)
+    x_str_r=x_str[::-1]    
+    return int(x_str_r)
 
+def isPrime(x):
+    res = 1  
+    for i in range(2,x):
+        if x % i == 0:
+            res = 0
+    if x == 1:
+        res = 0
+    return res;    
+
+n = int(input())
+li = list(map(int,input().split()))
+
+for i in range(n):
+    int_r = reverse(li[i])
+    if isPrime(int_r):
+        print(int_r)
 # 에라토스테네스체
 
 # 특정 수의(2이상부터) 배수는 소수가 아닌게 확정임으로 미리 체크를하고
 
-def isPrime(x):
-    res = 1  # 0 이 되면 소수
-    for i in range(2,x):
-        if x % i == 0:
-            print("i=",i)
-            res = 0
-
-    return res;    
-    
-
-print(isPrime(5))
-print(isPrime(3))
-print("1212")
-print(isPrime(12))
+# print(isPrime(5))
+# print(isPrime(3))
+# print("1212")
+# print(isPrime(12))
 
