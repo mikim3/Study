13171907b560ed0ccf515bd6ab139fa8cb551c1b
김민수 <minsu980107@naver.com>
2022-07-31 20:37:12 +0900

    사과나무

--- PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 3/7. 사과나무/AA.py
+++ PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 3/7. 사과나무/AA.py
@@ -1,23 +1,45 @@
 #  1104 시작  11:44
 
-n = int(input());
+# 더 간단
 
-li = []
+n = int(input())
+a = [list(map(int,input().split())) for _ in range(n)]
+
+res = 0
+s = e = n//2
 
 for i in range(n):
-    li.append(list(map(int, input().split()))) 
+    for j in range(s, e+1):
+        res += a[i][j]
+    if i < n//2:
+        s -= 1
+        e += 1
+    else:
+        s += 1
+        e -= 1
+print(res)
 
-sum = 0
-for i in range(0,n):
-    if (i <= (n//2)):
-        for j in range((n//2)-i,n-(n//2) + i):
-            sum += li[i][j]
-    else: 
-        for j in range(i - (n//2),n + (n//2) - i):
-            sum += li[i][j]
 
 
-print(sum)
+
+
+
+# 내 풀이
+
+# n = int(input());
+
+# li = []
+# for i in range(n):
+#     li.append(list(map(int, input().split()))) 
+# sum = 0
+# for i in range(0,n):
+#     if (i <= (n//2)):
+#         for j in range((n//2)-i,n-(n//2) + i):
+#             sum += li[i][j]
+#     else: 
+#         for j in range(i - (n//2),n + (n//2) - i):
+#             sum += li[i][j]
+# print(sum)
 
 
 
