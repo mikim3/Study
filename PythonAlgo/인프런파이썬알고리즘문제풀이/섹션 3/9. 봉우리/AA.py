779cdb479084b115a8c5c012052132f9659ec0b5
김민수 <minsu980107@naver.com>
2022-08-05 23:35:11 +0900

    봉우리 풀기

---
+++ PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 3/9. 봉우리/AA.py
@@ -0,0 +1,17 @@
+# 11 21 시작 11 34 끝
+
+n = int(input())
+li = []
+
+li.append([0]*(n+2))
+for i in range(n):
+    li.append([0] + list(map(int,input().split())) + [0])
+li.append([0]*(n+2))
+
+bon = 0
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        if li[i][j] > li[i-1][j] and li[i][j] > li[i+1][j] and li[i][j] > li[i][j-1] and li[i][j] > li[i][j+1]:
+            bon += 1    
+
+print(bon)
