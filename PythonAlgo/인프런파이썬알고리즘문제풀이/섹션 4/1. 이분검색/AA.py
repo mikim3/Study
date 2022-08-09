de4da93f7cc143970ade737dc2f8a49fd8ffba76
김민수 <minsu980107@naver.com>
2022-08-10 00:30:02 +0900

    이분탐색 이론 공부 적용

--- PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 4/1. 이분검색/AA.py
+++ PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 4/1. 이분검색/AA.py
@@ -1,8 +1,19 @@
-# 시작시간 :     종료시간 :  
+# 약 10분 소요
 
-
-
-
+n , m = map(int,input().split())
+a = list(map(int, input().split()))
+a.sort()
+lt = 0
+rt = n - 1
+while lt <= rt:
+    mid = (lt + rt) // 2
+    if a[mid] == m:
+        print(mid+1)
+        break
+    elif a[mid] > m:
+        rt = mid - 1
+    else:
+        lt = mid + 1
 # 이분탐색 생각안한 무지성 코드
 
 # n , m = map(int,input().split())
