9276a38c81054f2971bf53aa7106af5e9ef1968e
김민수 <minsu980107@naver.com>
2022-08-09 22:11:52 +0900

    이분탐색

---
+++ PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 4/1. 이분검색/AA.py
@@ -0,0 +1,18 @@
+# 시작시간 :     종료시간 :  
+
+
+
+
+# 이분탐색 생각안한 무지성 코드
+
+# n , m = map(int,input().split())
+
+# li = []
+
+# li = list(map(int,input().split()))
+# li.sort()
+# for i in range(n):
+#     if li[i] == m:
+#         print(i+1)
+
+
