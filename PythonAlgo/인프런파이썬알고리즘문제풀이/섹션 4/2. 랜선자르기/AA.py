a693dcdf4ed71f207940e00c20b7572ac8347072
김민수 <minsu980107@naver.com>
2022-08-13 23:50:01 +0900

    랜선자르기(이분탐색,결정알고리즘)

--- PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 4/2. 랜선자르기/AA.py
+++ PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 4/2. 랜선자르기/AA.py
@@ -3,6 +3,27 @@
 k , n = map(int, input().split())
 
 li = []
-for i in range(n):
+for i in range(k):
     li.append(int(input()))
 
+max_value = max(li)
+max_index = li.index(max_value)
+
+lt = 0
+rt = max_value 
+div = 0
+mid = (lt + rt) // 2
+
+while lt <= rt:
+    for i in range(len(li)):
+        div += li[i]//mid
+    if div <= n:
+        rt = mid - 1
+    elif div > n:
+        lt = mid + 1
+        res = div
+        
+    
+print(res)    
+    
+    
