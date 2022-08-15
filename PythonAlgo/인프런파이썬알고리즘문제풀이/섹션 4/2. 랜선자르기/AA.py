c2410fb49f7fd1bf891d4500c3d47982c1973c6a
김민수 <minsu980107@naver.com>
2022-08-15 11:27:15 +0900

    랜선 자르기

--- PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 4/2. 랜선자르기/AA.py
+++ PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 4/2. 랜선자르기/AA.py
@@ -1,4 +1,13 @@
-# 시작시간 :  11 25   종료시간 :  
+# 시작시간 :  11 40   종료시간 :  12:20  
+
+# 갯수 조건에 맞는 경우를 찾는다.   크기를 키우면서 확인한다.
+
+#
+def Count(len):
+    cnt = 0
+    for x in li:
+        cnt += (x//len)
+    return cnt
 
 k , n = map(int, input().split())
 
@@ -7,23 +16,26 @@ for i in range(k):
     li.append(int(input()))
 
 max_value = max(li)
-max_index = li.index(max_value)
 
-lt = 0
+lt = 1
 rt = max_value 
+# 랜선 갯수
 div = 0
-mid = (lt + rt) // 2
+# 이분탐색의 중간값이자 랜선 갯수를 알아내는 수
 
 while lt <= rt:
-    for i in range(len(li)):
-        div += li[i]//mid
-    if div <= n:
+    mid = (lt + rt) // 2
+    # 랜선 갯수가 n보다 크면  너무 작게 잘랐으니 lt를 옮겨서 mid도 키워서 자르는 랜선의 크기를 높인다.  
+    if Count(mid)>=n:        
+        res = mid
+        lt = mid + 1  #
+    # 너무 크게 잘랐으니 더 작게 자른다.
+    else:
         rt = mid - 1
-    elif div > n:
-        lt = mid + 1
-        res = div
-        
-    
-print(res)    
-    
+
+print(res)
+
+
+
+
     
