2506d3b4d7d0f248db14d31d0cf144e6fb915774
김민수 <minsu980107@naver.com>
2022-08-16 16:53:20 +0900

    마구간 정하기

--- PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 4/4. 마구간 정하기/AA.py
+++ PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 4/4. 마구간 정하기/AA.py
@@ -1,2 +1,39 @@
-# 시작시간 :     종료시간 :  
+# 시작시간 :  3:50     종료시간 : 4시 30
+
+from tkinter import E
+
+
+def Count(len):
+    cnt = 1
+    ep = li[0]
+    for i in range(1,n):
+        if li[i] - ep >= len:
+            cnt += 1
+            ep = li[i]
+    return cnt
+    
+      
+n , c = map(int, input().split())
+li = []
+
+for i in range(n):
+    li.append(int(input()))
+
+li.sort()
+
+lt = 1
+rt = li[n-1]
+mid = 0
+
+while lt <= rt:
+    mid = (lt + rt) // 2
+    if Count(mid) >= c:
+        res = mid
+        lt = mid + 1
+    else:
+        rt = mid - 1
+print(res)
+
+
+    
 
