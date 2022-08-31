057c2c62ab6f010051c9055935dd99d84098ee89
김민수 <minsu980107@naver.com>
2022-08-31 22:45:12 +0900

    코테준비

--- PythonAlgo/백준알고리즘풀기/백준단계별풀어보기/9단계재귀함수/하노이 탑 이동순서_11729.py
+++ PythonAlgo/백준알고리즘풀기/백준단계별풀어보기/9단계재귀함수/하노이 탑 이동순서_11729.py
@@ -1,4 +1,22 @@
-# 11729 번 문제
-# 하노이 탑 이동 순서
+# 시작시간: 10:36  종료시간:  11:02
 
+# 3가지 과정으로 나눠서 생각할 수 있다.
 
+# 1단계: n-1개의 원판을 2번막대로 옮긴다.
+
+# 2단계: 남은 1개의 원판을 1번 막대에서 3번 막대로 옮긴다.
+
+# 3단계: n-1개의 원판을 2번 막대에서 3번 막대로 옮긴다.
+
+def hanoi_tower(n, start, end):
+    if n == 1:
+        print(start, end)
+        return
+    hanoi_tower(n-1, start, 6-start-end)  # 1단계
+    print(start, end) # 2단계
+    hanoi_tower(n-1, 6-start-end, end)  # 3단계   
+
+n = int(input())
+
+print(2**n-1)
+hanoi_tower(n , 1 , 3)
