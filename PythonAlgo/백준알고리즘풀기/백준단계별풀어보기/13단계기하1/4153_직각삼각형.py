e90fc9aaf654a475e698be64bea6437030b8dd71
김민수 <minsu980107@naver.com>
2022-09-19 23:44:21 +0900

    Update 4153_직각삼각형.py

--- PythonAlgo/백준알고리즘풀기/백준단계별풀어보기/13단계기하1/4153_직각삼각형.py
+++ PythonAlgo/백준알고리즘풀기/백준단계별풀어보기/13단계기하1/4153_직각삼각형.py
@@ -1,9 +1,9 @@
-# 11:37 시작
+# 11:37 시작 11:44끝
 
 a, b, c = map(int,input().split())
 
 while a+b+c > 0:
-    if a**2 + b**2 == c**2:
+    if a**2 + b**2 == c**2 or a**2 + c**2 == b**2 or b**2+c**2 == a**2:
         print("right")
     else:
         print("wrong")
