392a80e5fa05c4ba42b1849c290283c6d1c8a2ba
김민수 <minsu980107@naver.com>
2022-09-19 23:31:06 +0900

    Update 11478_서로다른부분문자열의개수.py

--- PythonAlgo/백준알고리즘풀기/백준단계별풀어보기/12단계집합과맵/11478_서로다른부분문자열의개수.py
+++ PythonAlgo/백준알고리즘풀기/백준단계별풀어보기/12단계집합과맵/11478_서로다른부분문자열의개수.py
@@ -1,4 +1,4 @@
-#
+# 11:10 시작  11:22 끝
 
 # ababc의 부분 문자열은 a, b, a, b, c, ab, ba, ab, bc, aba, bab, abc, abab, babc, ababc가 있고, 서로 다른것의 개수는 12개이다.
 
@@ -6,9 +6,22 @@
 
 li = input()
 
-for i in range(len(li)): # 글자의 길이만큼 반복한다.
-    for j in range(len(li)):
-
-
+# 만든 글자를 임시로 저장
+st = ""
+# dic 
+dic = {}
+# 
+total = 0
+for i in range(len(li)): # 글자의 길이 경우의 수 만큼 반복한다.
+    for j in range(len(li) - i):  # 앞에 글자부터 한글자씩 만든다
+        st += li[j:j+i+1]
+        total += 1
+        if st in dic:
+            dic[st] += 1
+        else:
+            dic[st] = 0
+        st=""
+    
+print(total - sum(dic.values()))
 
 
