42b49f99347910459521e411c69eb0059d5b9c5b
김민수 <minsu980107@naver.com>
2022-09-24 17:40:49 +0900

    3085푸는중

---
+++ PythonAlgo/백준알고리즘풀기/codeplus/브루트포스/3085.py
@@ -0,0 +1,26 @@
+# 시작시간  :   15:53분
+
+# 최대 갯수 좌표(공동 1위 포함)들을 구한다.
+
+n = int(input())
+
+li_candy = []
+for i in range(n):
+    li_candy.append(list(map(str,input())))
+
+# 현재 좌표 i,j 
+# 
+for i in range(n):
+    for j in range(n):
+        # x좌표 비교
+        for x in range(n-j):
+            if li_candy[i][j] == : 
+            
+        # y좌표 비교
+        for y in range(n-i):
+
+print(li_candy)
+
+
+
+
