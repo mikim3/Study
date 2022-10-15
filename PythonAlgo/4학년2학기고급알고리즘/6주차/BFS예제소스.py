f9b88c56ece5478993507dcc4e4acd6e1f5079c9
rlaalstn0107 <minsu980107@naver.com>
2022-10-15 22:55:50 +0900

    학교알고리즘 복습

--- PythonAlgo/4학년2학기고급알고리즘/6주차/BFS예제소스.py
+++ PythonAlgo/4학년2학기고급알고리즘/6주차/BFS예제소스.py
@@ -3,14 +3,16 @@ from collections import deque
 
 # 너비우선탐색
 def bfs(graph, start, visited):
-    # 
+    # 큐에 시작할 노드 넣고 시작
     queue = deque([start])
+    # 1번노드는 방문했다고 표시
     visited[start] = True
-    # 큐를 다 pop할 때까지 반복
+    # 큐를 다 pop해서 방문 안한 노드가 없을때까지 반복
     while queue:
         # 왼쪽부터 pop함으로써 큐처럼 사용
         v = queue.popleft()
         print(v, end=' ')
+        # graph에서 해당 노드와 인접한 노드 탐색  graph[1] == [2,3,8]
         for i in graph[v]:
             if not visited[i]:
                 queue.append(i)
---
+++ PythonAlgo/4학년2학기고급알고리즘/6주차/BFS혼자만들어보기.py
@@ -0,0 +1,48 @@
+# 07:54 시작
+
+# BFS과정
+# 1. 탐색 시작 노드를 큐에 삽입하고 방문처리를 한다.
+# 2. 큐에서 노드를 꺼낸 뒤에 해당 노드의 인접 노드 중에서 
+# 방문하지 않은 노드를 모두 큐에 삽입하고 방문 처리를 한다.
+# 3. 더 이상 2번의 과정을 수행할 수 없을 때까지 반복한다.
+
+from collections import deque
+import queue
+
+
+
+def bfs(graph,start,visited):
+    # 큐 초기화
+    queue = deque([start])
+    # 
+    visited[start] = True
+    
+    while queue:
+        v = 
+        
+        for i in graph[v]:
+            
+            if not visited[v]:
+                
+                visited[v] = True
+
+
+
+
+start = 1
+
+visited = [False] * 9
+
+graph = [
+    [],
+    [2,3,8],
+    [1,7],
+    [1,4,5],
+    [3,5],
+    [3,4],
+    [7],
+    [2,6,8],
+    [1,7]
+]
+
+
