4d71eafe2b8d3f9bc7232c12446999e1e55b22b3
김민수 <minsu980107@naver.com>
2022-10-17 22:20:03 +0900

    테스트케이스 추가

--- PythonAlgo/4학년2학기고급알고리즘/6주차/BFS예제소스.py
+++ PythonAlgo/4학년2학기고급알고리즘/6주차/BFS예제소스.py
@@ -16,17 +16,24 @@ def bfs(graph, start, visited):
         for i in graph[v]:
             if not visited[i]:
                 queue.append(i)
-                visited[i] = True 
+                visited[i] = True
+# graph = [
+#     [],
+#     [2,3,8],
+#     [1,7],
+#     [1,4,5],
+#     [3,5],
+#     [3,4],
+#     [7],
+#     [2,6,8],
+#     [1,7]
+# ]
 graph = [
     [],
-    [2,3,8],
-    [1,7],
-    [1,4,5],
-    [3,5],
-    [3,4],
-    [7],
-    [2,6,8],
-    [1,7]
+    [2,3,2],
+    [1,4],
+    [1,4],
+    [1,2,3]
 ]
 # 방문 여부 초기화
 visited = [False] * 9
