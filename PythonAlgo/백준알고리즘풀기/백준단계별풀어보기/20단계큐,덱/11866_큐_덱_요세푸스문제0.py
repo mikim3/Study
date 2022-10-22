14ed95b7c0599bdc3c6ba472ecc7f3513b047c4d
김민수 <minsu980107@naver.com>
2022-10-22 15:50:57 +0900

    11866 다시풀기

--- PythonAlgo/백준알고리즘풀기/백준단계별풀어보기/20단계큐,덱/11866_큐_덱_요세푸스문제0.py
+++ PythonAlgo/백준알고리즘풀기/백준단계별풀어보기/20단계큐,덱/11866_큐_덱_요세푸스문제0.py
@@ -4,16 +4,16 @@
 
 from collections import deque
 n, k = map(int, input().split())
-s = deque([])
-for i in range(1, n + 1):
-    s.append(i)
-print('<', end='')
-while s:
-    for i in range(k - 1):
-        s.append(s[0])
-        s.popleft()
-    print(s.popleft(), end='')
-    if s:
-        print(', ', end='')
-    print(s)
-print('>')
+queue = deque([])
+
+for i in range(1,n+1):
+    queue.append(i)
+
+print("<",end="")
+while len(queue)>=2:
+    for i in range(k-1):
+        queue.append(queue[0])
+        queue.popleft()
+    print(queue.popleft(),end=", ")
+print(queue.popleft(),end="")
+print(">",end="")
