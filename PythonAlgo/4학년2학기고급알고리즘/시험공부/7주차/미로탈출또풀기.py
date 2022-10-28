217e0240a1f08d90a8e30e75ef4ffce275493012
rlaalstn0107 <minsu980107@naver.com>
2022-10-28 18:08:59 +0900

    학교 알고리즘 공부

--- PythonAlgo/4학년2학기고급알고리즘/시험공부/7주차/미로탈출또풀기.py
+++ PythonAlgo/4학년2학기고급알고리즘/시험공부/7주차/미로탈출또풀기.py
@@ -3,34 +3,39 @@
 
 from collections import deque
 
+#visited 는 값이 graph 좌표가 1이냐로 대체 된다
+
 def bfs(x,y):
+
     queue = deque()
-    # 첫 좌표 (x,y)
     queue.append((x,y))
     while queue:
         x,y = queue.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if nx < 0 or nx >= n or ny < 0 or ny >= m:
+            if nx < 0 or ny < 0 or nx >= n or ny >= m:
                 continue
             if graph[nx][ny] == 0:
                 continue
             if graph[nx][ny] == 1:
                 graph[nx][ny] = graph[x][y] + 1
                 queue.append((nx,ny))
-    return graph[n-1][m-1]
+    return graph[n - 1][m - 1]
+    
+    
+    
 
+
+
+# 상하좌우
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
 n,m = map(int,input().split())
+
 graph = []
 for i in range(n):
     graph.append(list(map(int,input())))
 
-#상,하,좌,우
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
-
 print(bfs(0,0))
-
-
 
