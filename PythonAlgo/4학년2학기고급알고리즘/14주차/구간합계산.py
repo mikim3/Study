678fd09c2e808e25bc8e609a8497a49bb9e4a32a
rlaalstn0107 <minsu980107@naver.com>
2022-12-14 23:57:55 +0900

    11659구간합계산 시간초과계선

--- PythonAlgo/4학년2학기고급알고리즘/14주차/구간합계산.py
+++ PythonAlgo/4학년2학기고급알고리즘/14주차/구간합계산.py
@@ -1,20 +1,49 @@
 #   10 20 30 40  50
 # 0 10 30 60 100 150
 
-arr = list(map(int,input().split()))
 
-print(arr)
+import sys
+input = sys.stdin.readline
+ 
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+prefix_sum = [0]    # init prefix_sum    
+ 
+temp = 0    
+for i in arr:    # accumulate arr section 
+    temp += i
+    prefix_sum.append(temp)
+ 
+for i in range(m):
+    a, b = map(int, input().split())
+    print(prefix_sum[b] - prefix_sum[a-1])
 
-arr_sum = []
-arr_sum.append(0)
-sum = 0
-for i in range(0,len(arr)):
-    sum += arr[i]
-    arr_sum.append(sum)    
-print(arr_sum)
 
-left = 0
-right = 0
+
+
+
+
+
+
+
+
+
+## 시간초과!!
+
+# arr = list(map(int,input().split()))
+
+# print(arr)
+
+# arr_sum = []
+# arr_sum.append(0)
+# sum = 0
+# for i in range(0,len(arr)):
+#     sum += arr[i]
+#     arr_sum.append(sum)    
+# print(arr_sum)
+
+# left = 0
+# right = 0
 
 
 
