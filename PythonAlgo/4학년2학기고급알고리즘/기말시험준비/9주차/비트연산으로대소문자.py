894da5786bc927c46414dadd77d4e08bb2be00e4
rlaalstn0107 <minsu980107@naver.com>
2023-01-02 00:06:33 +0900

    폴더정리

--- PythonAlgo/4학년2학기고급알고리즘/기말시험준비/9주차/비트연산으로대소문자.py
+++
@@ -1,8 +0,0 @@
-## 32차이
-# n = int(input())
-# print(n^32)
-sentence = input() 
-
-for i in range(len(sentence)):
-    print(chr(ord(sentence[i])^32),end = '')
-    
--- PythonAlgo/4학년2학기고급알고리즘/중간고사이전/시험공부/7주차/Counter.py
+++
@@ -1,8 +0,0 @@
-
-
-from collections import Counter
-
-count = Counter(['read','blus','blaco','blus','blus','read'])
-print(count)
-print(count['read'])
-print(dict(count))
