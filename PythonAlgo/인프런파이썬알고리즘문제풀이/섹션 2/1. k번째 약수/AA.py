88c18f231e56d5004651744ae772ab824a69f8b9
minsukim <minsu980107@naver.com>
2023-04-29 23:50:59 +0900

    구현 2문제

--- PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 2/1. k번째 약수/AA.py
+++ PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 2/1. k번째 약수/AA.py
@@ -1,12 +1,27 @@
-# 
-n, k = map(int,input().split())
 
+n, k = map(int, input().split())
 count = 0
-for i in range(1,n+1):
-    if n % i == 0:
+
+for i in range(1, n + 1):
+    if (n % i == 0):
         count += 1
-        if count == k:
+        if (count == k):
             print(i)
+            count = -1
             break
-else:  # for ... else: 정상적으로 끝났을시 else구문 작동
-    print(-1)    
+if (count != -1):
+    print(-1)
+
+### 다시풀기전
+# # 
+# n, k = map(int,input().split())
+
+# count = 0
+# for i in range(1,n+1):
+#     if n % i == 0:
+#         count += 1
+#         if count == k:
+#             print(i)
+#             break
+# else:  # for ... else: 정상적으로 끝났을시 else구문 작동
+#     print(-1)    
--- PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 2/2. K번째 수/AA.py
+++ PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 2/2. K번째 수/AA.py
@@ -1,8 +1,23 @@
+# 약 15분걸림
 
-t=int(input())
+t = int(input())
 
 for i in range(t):
-    n,s,e,k = map(int,input().split())
+    li = []
+    n,s,e,k = map(int, input().split())
     li = list(map(int,input().split()))
-    li_slice = sorted(li[s-1:e])
-    print("#%d %d"%(i+1,li_slice[k-1]))    
+    li_cmp = []
+    for j in range(s - 1,e):
+        li_cmp.append(li[j])
+    li_cmp.sort()
+    print("#" + str(i+1) + " "+ str(li_cmp[k-1]))
+    
+### 다시풀기전
+
+# t=int(input())
+
+# for i in range(t):
+#     n,s,e,k = map(int,input().split())
+#     li = list(map(int,input().split()))
+#     li_slice = sorted(li[s-1:e])
+#     print("#%d %d"%(i+1,li_slice[k-1]))    
