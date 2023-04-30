d60953f36c8ca50be0dc5bf38bd34e19680f03fc
김민수 <minsu980107@naver.com>
2023-04-30 20:38:07 +0900

    코딩테스트 구현유형

--- PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 2/3. k번째 큰 수/AA.py
+++ PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 2/3. k번째 큰 수/AA.py
@@ -1,11 +1,32 @@
+# 17분 걸림
+# 
+# for j in range(i + 1,n):  # i + 1이 아니라 그냥 1부터로 실수함   
 
-n, k = map(int,input().split())
+# print(li_sum[k - 1]) # k
+
+
+n , k = map(int, input().split())
+
 li = list(map(int,input().split()))
+set_sum = set()
+
+for i in range(n):
+    for j in range(i + 1,n):
+        for z in range(j + 1,n):
+            set_sum.add(li[i]+li[j]+li[z])
 li_sum = []
-for i in range(len(li)):
-    for j in range(i+1,len(li)):
-        for x in range(j+1,len(li)):
-            li_sum.append(li[i] + li[j] + li[x])    
-li_sum_set=list(set(li_sum))
-li_sum_set.sort(reverse=True)
-print(li_sum_set[k-1])
+
+li_sum = sorted(set_sum,reverse=True)
+
+print(li_sum[k - 1])
+
+# n, k = map(int,input().split())
+# li = list(map(int,input().split()))
+# li_sum = []
+# for i in range(len(li)):
+#     for j in range(i+1,len(li)):
+#         for x in range(j+1,len(li)):
+#             li_sum.append(li[i] + li[j] + li[x])    
+# li_sum_set=list(set(li_sum))
+# li_sum_set.sort(reverse=True)
+# print(li_sum_set[k-1])
--- PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 2/4. 대표값/AA.py
+++ PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 2/4. 대표값/AA.py
@@ -1,19 +1,60 @@
-# 답봄
+# 19:50  시작
+# 20:35  마무리
+
+# 답지를 봤으나 알아서 풀음
+# 그냥 구현인데 쉽게 생각해내지 못했음
+
 n = int(input())
 li = list(map(int,input().split()))
+sum_grade = 0
+avg = 0
 
-av = int(sum(li)/n + 0.5)
-min = 2147000000
+for i in range(n):
+    sum_grade += li[i]
 
-for idx, x in enumerate(li):
-    tmp = abs(x-av)
-    if tmp<min:
-        min = tmp
-        score = x
-        res = idx+1
-    elif tmp == min:
-        if x > score:
-            score = x
-            res = idx + 1
-print(av, res)
+avg = round(sum_grade / n) # 소수점
+
+check = 0 # 출력해야할 가장 차이가 적은 학생의 인덱스
+# li_check = [] # 공동 1등 일단 체크
+min = 22222222222
+check_value = 0
+for i in range(n):
+    if (abs(li[i]-avg) < min):
+        check = i
+        check_value = li[i]
+        min = abs(li[i]-avg)
+    elif (abs(li[i]-avg) == min):
+        if (li[i] > check_value):
+            check = i
+            check_value = li[i]
+            min = abs(li[i]-avg)
+
+
+print(avg, check + 1)
+
+
+
+
+# # 답봄
+# n = int(input())
+# li = list(map(int,input().split()))
+
+# av = int(sum(li)/n + 0.5)
+# min = 2147000000
+
+# for <원소> in enumrate(<목록>):
+# for <원소1(인덱스)> <원소2(값)> in enumrate(<목록>):
+# for <원소1(인덱스)> <원소2(값)> in enumrate(<목록>, start = 1):
+
+# for idx, x in enumerate(li):
+#     tmp = abs(x-av)
+#     if tmp<min:
+#         min = tmp
+#         score = x #현재 근접한 점수
+#         res = idx+1
+#     elif tmp == min:
+#         if x > score:
+#             score = x
+#             res = idx + 1
+# print(av, res)
 
