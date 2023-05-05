972e5c27e0110eed3bed5339f3534ab326c09dc6
김민수 <minsu980107@naver.com>
2023-05-05 21:32:09 +0900

    알고리즘 스택문제공부

--- PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 5/1. 가장 큰 수/AA.py
+++ PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 5/1. 가장 큰 수/AA.py
@@ -5,10 +5,24 @@
 # m 개를 지울수 있다 == pop핤수 있다.
 # 
 
+# 스택에 맨  앞앞과  비교 후 본인보다 작작으으면  pop
+# pop 갯수를 다쓰면 그냥 스택에 쌓고 마무리
+
 
 n,m = map(int,input().split())
 
-for i in range(len(str(n))):
+n_str = str(n) # 입력받은거 str로 변환
+li = [] # 비교할 스택 
+# 
+ret = ''
+li.append(n_str[0]) 
+for i in range(1,len(n_str)):
+    if (li[-1] < n_str[i]):
+        li.pop()
+    else:
+        li.append(n_str[i]) 
+
+print(li)
 
 
 
