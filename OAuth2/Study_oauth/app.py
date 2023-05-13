edb997489d0761ab1761aaf83ae40e3730e4a005
minsukim <minsu980107@naver.com>
2023-05-13 15:12:27 +0900

    OAuth2.0공부코드

--- OAuth2/Study_oauth/app.py
+++ OAuth2/Study_oauth/app.py
@@ -21,6 +21,9 @@ def index4():
 @app.route('/index5')  
 def index5():
     return render_template('index5.html')
+@app.route('/index6')  
+def index6():
+    return render_template('index6.html')
 
 if __name__ == '__main__':
     app.debug = True
