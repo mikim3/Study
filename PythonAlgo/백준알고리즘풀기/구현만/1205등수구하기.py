88d1900e6d6ff9aa7487be1fa4dfdaa30b199c5a
김민수 <minsu980107@naver.com>
2023-06-18 21:24:35 +0900

    1205등수구하기

---
+++ PythonAlgo/백준알고리즘풀기/구현만/1205등수구하기.py
@@ -0,0 +1,19 @@
+# 21:10시작
+
+# 공동등수는 높은 등수로 모두 표현\
+#
+
+# 주어진 점수갯수, 태수의 점수 , 랭킹리스트에 올릴수 점수의 개수
+n, new_score, p = map(int,input().split())
+# 현재 랭킹리스트
+li = list(map(int,input().split()))
+
+li.sort(reverse=True)
+print(li)
+
+#
+rank = 0
+for i in range(n):
+    if (new_score > ):
+
+
