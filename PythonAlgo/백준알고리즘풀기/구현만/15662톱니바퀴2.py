413a38c3413ee736336fb5af82f0a41b2f9f83d9
김민수 <minsu980107@naver.com>
2023-06-22 22:11:43 +0900

    Update 15662톱니바퀴2.py

--- PythonAlgo/백준알고리즘풀기/구현만/15662톱니바퀴2.py
+++ PythonAlgo/백준알고리즘풀기/구현만/15662톱니바퀴2.py
@@ -1,4 +1,4 @@
-# 230622 17:44시작
+# 230622 17:44시작 ~ 18:40 마무리
 
 # 톱니 A가 돌았을떄 그옆에 A가 맞닿은 톱니의 극이 다르다면
 # B는 A가 회전한 방향과 반대방향으로 회전하게 된다.
@@ -14,12 +14,64 @@
 # 회전방법은 $(몇번째 바퀴 회전) $(어떤 방향회전?) 1는 시계방향 -1는 반시계방향이다.
 #
 
+# 회전하기 전에 옆에도 같이 회전할지를 결정한다.  회전 한 후에 결정X
+#
+
+# 옆에서 회전한거의 반대방향으로 회전한다.
+
+
+# import sys
+# input = sys.stdin.readline
+
 # 톱니
 t = int(input())
+li = []
 for i in range(t):
-    li
-
+  li.append(list(input()))
+# print(li, "\n")
 
 # 회전횟수
 k = int(input())
+
+#
+def rotate_t(t_select, dir, li):
+  if (dir == 1):
+    # print("dir == 1")
+    # 한칸씩 뒤로 민다.
+    tmp = li[t_select][7]
+    for i in range(7, 0, -1):
+      li[t_select][i] = li[t_select][i - 1]
+    li[t_select][0] = tmp
+    # print("한칸씩 뒤로밈", li)
+  # 반시계방향
+  elif (dir == -1):
+    tmp = li[t_select][0]
+    for i in range(7):
+      li[t_select][i] = li[t_select][i+1]
+    li[t_select][7] = tmp
+    # print("한칸씩 앞으로 당김", li)
+  print("rotate_t 결과", li)
+
+for i in range(k):
+  t_select, dir = map(int, input().split())
+  t_select = t_select - 1
+  # 단순히 돌리라고 명령한 톱니
+  # 시계방향
+
+  if (li[t_select][2] == li[t_select + 1][6]):
+
+  rotate_t(t_select, dir, li)
+  print("rotate 직후",li)
+  # 0 1 [2] 3 4 5 [6] 7 확인해야함
+
+
+
+# print("밀기끝", li)
+
+su = 0
+for i in range(t):
+  if li[i][0] == '1':
+    su += 1
+print(su)
+
 
