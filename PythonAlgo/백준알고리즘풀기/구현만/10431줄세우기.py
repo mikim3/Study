2a3ffdbb5bf403f9022f5e5787ce3c50cc4f44b4
rlaalstn0107 <minsu980107@naver.com>
2023-06-26 23:59:10 +0900

    Create 10431줄세우기.py

---
+++ PythonAlgo/백준알고리즘풀기/구현만/10431줄세우기.py
@@ -0,0 +1,20 @@
+# 테케이스   뒤로 물러난 걸음수
+
+# sort를 하는데 sort가 주어진 방법 그대로 사용하여서 sort하자
+# 0번부터 시작해서 마지막까지 반복
+# 자기 앞에 자기보다 큰사람이 있다면 그 중 가장 큰사람 앞으로 간다.
+
+p = int(input())
+
+for i in range(p):
+    li = list(map(int,input()))
+    for j in range(1,21):
+        if :
+
+
+
+
+
+
+
+
