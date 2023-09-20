eab77ef3afa1fd979b6ae40cf109c06f622c95e7
김민수 <minsu980107@naver.com>
2023-09-20 16:28:33 +0900

    feat(구현): 2669직사각형의합집합

--- PythonAlgo/백준알고리즘풀기/구현만/2669직사각형의합집합.py
+++ PythonAlgo/백준알고리즘풀기/구현만/2669직사각형의합집합.py
@@ -1,7 +1,20 @@
-# 시작 시간 230920 00:12 12:12
-# 답 봤음
+# 두번째 풀기 230920 14:43   14:52
 
+matrix = [[0] * 101 for _ in range(101)]
+# 0 이면 빈칸  1이면 차있는 칸
+for i in range(4):
+  x1, y1, x2, y2 = map(int, input().split())
+  for j in range(x1,x2):
+    for z in range(y1,y2):
+      matrix[j][z] = 1
 
+total = 0
+for i in range(101):
+  for j in range(101):
+    if (matrix[i][j] == 1):
+      total += 1
+
+print(total)
 
 
 
@@ -18,6 +31,8 @@
 
 
 # ---------------------------------------------
+# 시작 시간 230920 00:12 12:12
+# 답 봤음
 
 # 0으로 차있는 2차원 배열을 만든다.
 # 좌표안에 들어가면 1로 바꾼다.
@@ -25,23 +40,23 @@
 # 1갯수를 구한다.
 
 
-# [0] 이 101개 들어있는 배열 101개 만들기
-matrix = [[0] * 101 for _ in range(101)] 
-# print(matrix)
+# # [0] 이 101개 들어있는 배열 101개 만들기
+# matrix = [[0] * 101 for _ in range(101)]
+# # print(matrix)
 
-for i in range(4):
-  x1, y1, x2, y2 = map(int, input().split())
-  for j in range(x1, x2):
-    for z in range(y1, y2):
-      matrix[j][z] = 1
+# for i in range(4):
+#   x1, y1, x2, y2 = map(int, input().split())
+#   for j in range(x1, x2):
+#     for z in range(y1, y2):
+#       matrix[j][z] = 1
 
-total = 0
-for i in range(101):
-  for j in range(101):
-    if matrix[i][j] == 1:
-      total += 1
+# total = 0
+# for i in range(101):
+#   for j in range(101):
+#     if matrix[i][j] == 1:
+#       total += 1
 
-print(total)
+# print(total)
 
 
 
