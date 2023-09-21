1af6f424e360a72526318975752b8bd4031809c0
rlaalstn0107 <minsu980107@naver.com>
2023-09-21 23:27:03 +0900

    feat(스택): 스택문제 가장큰수

--- PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 5/1. 가장 큰 수/AA.py
+++ PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 5/1. 가장 큰 수/AA.py
@@ -3,30 +3,49 @@
 # 이 번에는 밑에 주석을 보고 풀어버림
 # 밑에 답안 인듯한 주석이 있어서 참고했는데 그냥 과거에 내가 틀린 소스였음 그래서 시간 더 걸림
 
-n, m = map(int, input().split())
-n_str = str(n)
-li_n = []
+# n, m = map(int, input().split())
+# n_str = str(n)
+# li_n = []
 
-li_n.append(n_str[0])
-count_pop = 0
-for i in range(1,len(n_str)):
-  if (count_pop != m):
-    for j in range(len(li_n) - 1,-1,-1):
-      if (n_str[i] > li_n[j]):
-        li_n.pop()
-        count_pop += 1
-        if (count_pop == m):
-          break
-  else:
-    for j in range(i,len(n_str)):
-      li_n.append(n_str[j])
-    break
-  # count_pop이 다 쌓이기 전에 append
-  li_n.append(n_str[i])
+# li_n.append(n_str[0])
+# count_pop = 0
+# for i in range(1,len(n_str)):
+#   if (count_pop != m):
+#     for j in range(len(li_n) - 1,-1,-1):
+#       if (n_str[i] > li_n[j]):
+#         li_n.pop()
+#         count_pop += 1
+#         if (count_pop == m):
+#           break
+#   else:
+#     for j in range(i,len(n_str)):
+#       li_n.append(n_str[j])
+#     break
+#   # count_pop이 다 쌓이기 전에 append
+#   li_n.append(n_str[i])
 
-if (count_pop != m):
-  for i in range(m - count_pop):
-    li_n.pop()
+# if (count_pop != m):
+#   for i in range(m - count_pop):
+#     li_n.pop()
 
-for i in range(len(li_n)):
-  print(li_n[i], end='')
+# for i in range(len(li_n)):
+#   print(li_n[i], end='')
+
+#####################
+# 선생님 답안
+
+num, m = map(int, input().split())
+num = list(map(int, str(num)))
+stack = []
+
+for x in num:
+  while stack and m > 0 and stack[-1] < x:
+    stack.pop()
+    m -= 1
+  stack.append(x)
+
+if m != 0:
+  stack = stack[:-m]
+
+res = ''.join(map(str, stack))
+print(res)
