fe139b55bc88d3fc248d4a12723956eb18889a45
rlaalstn0107 <minsu980107@naver.com>
2023-09-21 23:04:05 +0900

    feat(스택): 인프런 코테 강의 스택 문제풀기

--- PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 5/1. 가장 큰 수/AA.py
+++ PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 5/1. 가장 큰 수/AA.py
@@ -1,30 +1,32 @@
-##########################
-# 시작시간     마무리시간
+#############################
+# 다시품 시작시간 230921 22:01     마무리시간 23:01
+# 이 번에는 밑에 주석을 보고 풀어버림
+# 밑에 답안 인듯한 주석이 있어서 참고했는데 그냥 과거에 내가 틀린 소스였음 그래서 시간 더 걸림
 
-# 본인 앞에 있는게 자기보다 작으면 앞에 있는걸 pop한다.
-# m 개를 지울수 있다 == pop핤수 있다.
-# 
+n, m = map(int, input().split())
+n_str = str(n)
+li_n = []
 
-# 스택에 맨  앞앞과  비교 후 본인보다 작작으으면  pop
-# pop 갯수를 다쓰면 그냥 스택에 쌓고 마무리
-
-
-n,m = map(int,input().split())
-
-n_str = str(n) # 입력받은거 str로 변환
-li = [] # 비교할 스택 
-# 
-ret = ''
-li.append(n_str[0]) 
+li_n.append(n_str[0])
+count_pop = 0
 for i in range(1,len(n_str)):
-    if (li[-1] < n_str[i]):
-        li.pop()
-    else:
-        li.append(n_str[i]) 
+  if (count_pop != m):
+    for j in range(len(li_n) - 1,-1,-1):
+      if (n_str[i] > li_n[j]):
+        li_n.pop()
+        count_pop += 1
+        if (count_pop == m):
+          break
+  else:
+    for j in range(i,len(n_str)):
+      li_n.append(n_str[j])
+    break
+  # count_pop이 다 쌓이기 전에 append
+  li_n.append(n_str[i])
 
-print(li)
+if (count_pop != m):
+  for i in range(m - count_pop):
+    li_n.pop()
 
-
-
-
-#########################
+for i in range(len(li_n)):
+  print(li_n[i], end='')
