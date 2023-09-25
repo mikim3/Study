cadd2e5b6f4546d45fa79455ce7d8689a9f98db5
rlaalstn0107 <minsu980107@naver.com>
2023-09-25 21:34:31 +0900

    Feat(스택): 섹션5단어찾기

--- PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 5/8. 단어찾기/AA.py
+++ PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 5/8. 단어찾기/AA.py
@@ -1,11 +1,17 @@
 ##########################
-# 시작시간     마무리시간
+# 시작시간 21:27    마무리시간 21:33
 
+n = int(input())
+li = []
 
+for i in range(n):
+  li.append(input())
 
+for i in range(n-1):
+  input_word = input()
+  if input_word in li:
+    li.remove(input_word)
 
-
-
-
+print(li[0])
 
 #########################
