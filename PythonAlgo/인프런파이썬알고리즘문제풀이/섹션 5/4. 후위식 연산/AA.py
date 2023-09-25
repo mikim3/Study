888a34a9d0b6411afd8ea57d873f6d041886b101
rlaalstn0107 <minsu980107@naver.com>
2023-09-25 20:27:14 +0900

    FEAT(스택): 후위식연산

--- PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 5/4. 후위식 연산/AA.py
+++ PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 5/4. 후위식 연산/AA.py
@@ -1,11 +1,34 @@
 ##########################
-# 시작시간     마무리시간
+# 시작시간 19:21  마무리시간 19:39
+# 혼자품
+
+# 앞에서부터 계산
+# 연산자를 만나면 계산
+# 숫자만 만나면 append()
+# 연산자를 만나면 스택맨위에 2개를 pop해서 서로 연산
+# 연산결과를 맨위로 올림
+
+expression = input()
+stack = []
+res = 0
+
+for x in expression:
+  if x.isdigit():
+    stack.append(int(x))
+  else:
+    # expression에서 더 뒤에 있는 숫자
+    target1 = int(stack.pop()) 
+    target2 = int(stack.pop())
+    if x == '+':
+      stack.append(target2 + target1)
+    elif x == '-':
+      stack.append(target2 - target1)
+    elif x == '*':
+      stack.append(target2 * target1)
+    elif x == '/':
+      stack.append(target2 / target1)
 
 
-
-
-
-
-
+print(stack.pop())
 
 #########################
