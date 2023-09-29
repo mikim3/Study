bc52083d4ced2213d5e6875c386e7ad7fa00ea02
rlaalstn0107 <minsu980107@naver.com>
2023-09-29 15:47:34 +0900

    Feat(큐): 공주구하기

--- PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 5/5. 공주구하기/AA.py
+++ PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 5/5. 공주구하기/AA.py
@@ -5,30 +5,48 @@
 
 from collections import deque
 
-def delete_deque_to_index(deq, index):
-  tmp_li = list(deq)
-  tmp_li.pop(index)
-  return deque(tmp_li)
+# def delete_deque_to_index(deq, index):
+#   tmp_li = list(deq)
+#   tmp_li.pop(index)
+#   return deque(tmp_li)
 
-n, k = map(int, input().split())
-deq = deque()
-for i in range(1,n+1):
-  deq.append(i)
+# n, k = map(int, input().split())
+# # deq = deque()
+# # for i in range(1,n+1):
+# #   deq.append(i)
 
-# k랑 shoutNum이 일치할때 제외시킴
-shoutNum = 0
-while n > 1:
-  i = 0
-  while i < len(deq):
-    shoutNum+=1
-    if (shoutNum == k):
-      deq = delete_deque_to_index(deq, i)
-      i -= 1
-      n -= 1
-      shoutNum = 0
-    i += 1
+# deq = list(range(1, n+1))
+# deq = deque(deq)
+
+
+# # k랑 shoutNum이 일치할때 제외시킴
+# shoutNum = 0
+# while n > 1:
+#   i = 0
+#   while i < len(deq):
+#     shoutNum+=1
+#     if (shoutNum == k):
+#       deq = delete_deque_to_index(deq, i)
+#       i -= 1
+#       n -= 1
+#       shoutNum = 0
+#     i += 1
       
-print(deq[-1])
-
+# print(deq[-1])
 
 #########################
+# 인프런 소스
+
+n , k = map(int, input().split())
+dq = list(range(1, n + 1))
+dq = deque(dq)
+
+while dq:
+  for _ in range(k - 1):
+    # 아래 두줄이 원형으로 돌아가는 효과를 준다.
+    current = dq.popleft() 
+    dq.append(current)
+  dq.popleft()
+  if len(dq) == 1:
+    print(dq[0])
+    dq.popleft()
