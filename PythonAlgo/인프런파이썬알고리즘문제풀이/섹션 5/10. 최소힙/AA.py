5a16d7c0c31c75b06d9032ba27a687a8a845b110
rlaalstn0107 <minsu980107@naver.com>
2023-10-01 00:35:36 +0900

    Feat(힙) : 최소힙, 최대힙

--- PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 5/10. 최소힙/AA.py
+++ PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 5/10. 최소힙/AA.py
@@ -1,11 +1,20 @@
 ##########################
-# 시작시간     마무리시간
+# 그냥 강의 봄
 
+import heapq
 
-
-
-
-
-
+a = []
+while True:
+  n = int(input())
+  if n==-1:
+    break
+  if n==0:
+    if len(a) == 0:
+      print(-1)
+    else:
+      # print("yeah")
+      print(heapq.heappop(a))
+  else:
+    heapq.heappush(a,n)
 
 #########################
