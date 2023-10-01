05fa5d02470181e83499b74ebe46f52dab0939bf
rlaalstn0107 <minsu980107@naver.com>
2023-10-01 23:46:32 +0900

    Feat(재귀):이진수출력

--- PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 6/1. 재귀함수란(이진수출력)/AA.py
+++ PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 6/1. 재귀함수란(이진수출력)/AA.py
@@ -1,15 +1,30 @@
-##########################
-# 시작시간  0525 19:20   마무리시간
+########################
+# 시작시간 231001  1544 마무리 시간
+# 답봄
 
-def recur(x):
-    if x == 0:
-        return
-    else:
-        recur(x//2)
-        print(x % 2, end='')
+def recur(n):
+  if n == 0:
+    return
+  else:
+    recur(n // 2)
+    print(n % 2, end='' )
 
 n = int(input())
 recur(n)
+
+
+##########################
+# 시작시간  0525 19:20   마무리시간
+
+# def recur(x):
+#     if x == 0:
+#         return
+#     else:
+#         recur(x//2)
+#         print(x % 2, end='')
+
+# n = int(input())
+# recur(n)
 
 
 #########################
