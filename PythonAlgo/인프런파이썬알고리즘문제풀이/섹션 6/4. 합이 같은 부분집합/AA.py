3dc6ad957513a7711e49d9fbf39d0f72fafce285
mikim3 <minsu980107@naver.com>
2023-10-06 20:03:54 +0900

    Feat(DFS) : 합이 같은 부분집합

--- PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 6/4. 합이 같은 부분집합/AA.py
+++ PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 6/4. 합이 같은 부분집합/AA.py
@@ -1,43 +1,59 @@
-##########################
-# 시작시간 231005 21:50    마무리시간 22:13
-# 부분집한 구하기 소스를 보면서 풀음
-
-# 모든 부분집합을 구한다.
-
-# 구하는 과정에서 O는 집합1 X는 집합2에 append한다.
-
-# 각각의 합을 구한다.
-
+###########################
+# 예제 코드
 import sys
 
-def dfs(node):
-  if node == n+1:
-    for i in range(1, n+1):
-      if check[i] == 1:
-        li_1.append(li_input[i-1])
-      else:
-        li_2.append(li_input[i-1])
-    if sum(li_1) == sum(li_2):
-      # print("li_1,2",li_1 , "--------", li_2)
+def DFS(level, sum):
+  # 더 이상 진행할 필요가 없는경우
+  if sum > total / 2:
+    return
+  # 다 수행함
+  if level == n:
+    if sum == (total - sum):
       print("YES")
       sys.exit(0)
-    li_1.clear()
-    li_2.clear()
   else:
-    check[node] = 1
-    dfs(node + 1)
-    check[node] = 0
-    dfs(node + 1)
+    DFS(level+1, sum + li_input[level]) # a[level]이라는 값을 사용하는 경우 == 는 현재 레벨을 사용하는경우
+    DFS(level+1, sum) # 사용 안하는 경우
 
 n = int(input())
-li_input = list(map(int,input().split()))
-li_1 = []
-li_2 = []
-check = [0] * (n+1)
-dfs(1)
-
+li_input = list(map(int, input().split()))
+total = sum(li_input)
+DFS(0,0)
 print("NO")
 
 
+# ##########################
+# # 시작시간 231005 21:50    마무리시간 22:13
+# # 부분집한 구하기 소스를 보면서 풀음
+# # 해당문제를 푸는데는 내 소스가 교수님 소스보다 좋은듯
 
-#########################
+# import sys
+
+# def dfs(node):
+#   if node == n+1:
+#     for i in range(1, n+1):
+#       if check[i] == 1:
+#         li_1.append(li_input[i-1])
+#       else:
+#         li_2.append(li_input[i-1])
+#     if sum(li_1) == sum(li_2):
+#       # print("li_1,2",li_1 , "--------", li_2)
+#       print("YES")
+#       sys.exit(0)
+#     li_1.clear()
+#     li_2.clear()
+#   else:
+#     check[node] = 1
+#     dfs(node + 1)
+#     check[node] = 0
+#     dfs(node + 1)
+
+# n = int(input())
+# li_input = list(map(int,input().split()))
+# li_1 = []
+# li_2 = []
+# check = [0] * (n+1)
+# dfs(1)
+
+# print("NO")
+# #########################
