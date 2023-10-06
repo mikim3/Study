b3dae48a6784dc219c44d69051583ac0e15ae68a
mikim3 <minsu980107@naver.com>
2023-10-06 20:03:12 +0900

    Feat(DFS) : 부분집합 구하기 복습

--- PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 6/3. 부분집합 구하기/AA.py
+++ PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 6/3. 부분집합 구하기/AA.py
@@ -1,9 +1,6 @@
-##########################
-# 시작시간  231005    마무리시간
-# 답봄
-# 전위순회를 생각해야 되는 DFS문제
+#############################
+# 시작시간 231006 16:01 # 마무리시간 16:12
 
-# node는 현재 방문중인 노드
 def dfs(node):
   if node == n + 1:
     for i in range(1, n+1):
@@ -11,14 +8,36 @@ def dfs(node):
         print(i, end=' ')
     print()
   else:
-    # 아래 두줄이 묶여서 일단 1로 체크하고 다음 노드를 결정하는 기능이 구현됨 
     check[node] = 1
-    dfs(node + 1) # 
-    # 그후 0을 체크 
+    dfs(node + 1)
     check[node] = 0
     dfs(node + 1)
 
 n = int(input())
-check = [0] * (n + 1)
-dfs(1) 
+check = [0] * (n+1)
+dfs(1)
+
+##########################
+# 시작시간  231005    마무리시간
+# 답봄
+# 전위순회를 생각해야 되는 DFS문제
+
+# # node는 현재 방문중인 노드
+# def dfs(node):
+#   if node == n + 1:
+#     for i in range(1, n+1):
+#       if check[i] == 1:
+#         print(i, end=' ')
+#     print()
+#   else:
+#     # 아래 두줄이 묶여서 일단 1로 체크하고 다음 노드를 결정하는 기능이 구현됨
+#     check[node] = 1
+#     dfs(node + 1) #
+#     # 그후 0을 체크
+#     check[node] = 0
+#     dfs(node + 1)
+
+# n = int(input())
+# check = [0] * (n + 1)
+# dfs(1)
 
