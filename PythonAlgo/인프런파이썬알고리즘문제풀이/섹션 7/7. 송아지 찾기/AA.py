6995c6cfd830b62c2dcb9934da667c2c9985ded8
mikim3 <minsu980107@naver.com>
2023-10-06 21:40:20 +0900

    Feat(BFS) : 송아지찾기

--- PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 7/7. 송아지 찾기/AA.py
+++ PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 7/7. 송아지 찾기/AA.py
@@ -1,11 +1,32 @@
 ##########################
 # 시작시간     마무리시간
+# 바로 수업들음
 
+import sys
+from collections import deque
+MAX = 10000
+check = [0] * (MAX + 1)
+dis = [0]
+n, m = map(int, input().split())
+# 현재위치 지나간 곳이니까 체크
+check[n] = 1
+#
+dis[n] = 0
 
+#
+dQ = deque()
+dQ.append(n)
 
-
-
-
+while dQ:
+  now = dQ.popleft()
+  if now == m:
+    break
+  for next in(now - 1, now + 1, now + 5):
+    if 0 < next <= MAX:
+      if check[next] == 0:
+        dQ.append(next)
+        check[next] = 1
+        dis[next] = dis[now] + 1
 
 
 #########################
