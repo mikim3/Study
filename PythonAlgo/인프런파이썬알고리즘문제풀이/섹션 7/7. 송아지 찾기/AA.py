ddf57150d18d7d9540f1f8567709bd643e223c22
rlaalstn0107 <minsu980107@naver.com>
2023-12-17 10:42:15 +0900

    Feat(BFS): 송아지찾기

--- PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 7/7. 송아지 찾기/AA.py
+++ PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 7/7. 송아지 찾기/AA.py
@@ -1,32 +1,28 @@
 ##########################
-# 시작시간     마무리시간
+# 시작시간  231115 2345   마무리시간
 # 바로 수업들음
 
 import sys
 from collections import deque
-MAX = 10000
-check = [0] * (MAX + 1)
-dis = [0]
-n, m = map(int, input().split())
-# 현재위치 지나간 곳이니까 체크
-check[n] = 1
-#
-dis[n] = 0
 
-#
+MAX = 10001
+ch = [0] * (MAX+1) # 해당 위치를 지나갔는지 체크하는 배열
+dis = [0] * (MAX+1) # 해당 위치를 몇 번 만에 갔는지 기록하는 배열
+n, m = map(int, input().split())
+ch[n] = 1
+dis[n] = 0
 dQ = deque()
 dQ.append(n)
-
-while dQ:
-  now = dQ.popleft()
-  if now == m:
+while dQ: # 덱 비어있으면 멈춤
+  now = dQ.popleft() 
+  if now == m: # 목표지점 발견
     break
-  for next in(now - 1, now + 1, now + 5):
+  for next in (now-1, now+1, now+5): # 튜플값을 하나씩 뻗음
     if 0 < next <= MAX:
-      if check[next] == 0:
+      if ch[next] == 0:
         dQ.append(next)
-        check[next] = 1
+        ch[next] = 1
         dis[next] = dis[now] + 1
-
+print(dis[m])
 
 #########################
