c1c46a0d53c16a05fc411519452c1e5dced85290
mikim3 <minsu980107@naver.com>
2023-12-17 20:56:03 +0900

    Feat(구현): 섹션7 사과나무

--- PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 7/8. 사과나무/AA.py
+++ PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 7/8. 사과나무/AA.py
@@ -1,5 +1,31 @@
 ##########################
-# 시작시간     마무리시간
+# 시작시간  231217 20:15  마무리시간 20:55?
+
+n = int(input())
+li = []
+for i in range(n):
+  li.append(list(map(int, input().split())))
+
+print(li)
+su = 0
+for i in range(n):
+  if i <= n // 2:
+    for j in range(n//2-i, n//2+1+i):
+      su += li[i][j]
+  else:
+    for j in range(i - 2, n - i + 2):
+      su += li[i][j]
+
+print(su)
+
+#2
+#123
+#01234
+#123
+#2
+
+#n = 5
+#
 
 
 
