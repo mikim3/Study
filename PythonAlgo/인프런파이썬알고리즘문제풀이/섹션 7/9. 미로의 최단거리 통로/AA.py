a83a94401fa4b965202572a7f8e5b30ba76caf33
mikim3 <minsu980107@naver.com>
2023-12-18 19:38:24 +0900

    미로의최단거리통로 wip

--- PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 7/9. 미로의 최단거리 통로/AA.py
+++ PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 7/9. 미로의 최단거리 통로/AA.py
@@ -1,8 +1,18 @@
 ##########################
-# 시작시간     마무리시간
+# 시작시간  231218 17:38   6분소비  마무리시간
 
+# BFS
+# 주변에 갈수 있는 길이 있으면 덱큐에 넣음
+# 최소한으로 이동한 횟수 출력
 
+# 이미 지나간 곳 체크해야함
 
+from collections import deque
+
+matrix = []
+for i in range(7):
+  matrix.append(list(map(int, input().split())))
+print(matrix)
 
 
 
