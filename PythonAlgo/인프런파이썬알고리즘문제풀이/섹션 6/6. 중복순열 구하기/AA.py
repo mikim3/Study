83561440cca951da1ff89781bc414ae2fc714da1
mikim3 <minsu980107@naver.com>
2023-12-23 21:15:22 +0900

    Chore

--- PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 6/6. 중복순열 구하기/AA.py
+++ PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 6/6. 중복순열 구하기/AA.py
@@ -2,11 +2,29 @@
 # 시작시간  231223 1727  마무리시간 1824
 # '==' 을 써야 될 곳에 '>'을 써서 30분 날림 원래는 1754에 풀음
 
+# def DFS(level):
+#   if level == m:
+#     for i in range(m):
+#       print(result[i], end=' ')
+#     print()
+#   else:
+#     for i in range(n):
+#       result[level]= i+1
+#       DFS(level + 1)
+
+# n, m = map(int, input().split())
+# result = [0] * (n + 1)
+# DFS(0)
+
+#########################
+# 모범답안
 def DFS(level):
+  global count
   if level == m:
     for i in range(m):
       print(result[i], end=' ')
     print()
+    count+=1
   else:
     for i in range(n):
       result[level]= i+1
@@ -14,6 +32,7 @@ def DFS(level):
 
 n, m = map(int, input().split())
 result = [0] * (n + 1)
+count = 0
 DFS(0)
-
+print(count)
 #########################
