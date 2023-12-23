2d33b3da601ff0655d6a615f32604c8087c32874
mikim3 <minsu980107@naver.com>
2023-12-23 18:25:49 +0900

    Feat(DFS) : 섹션 6 중복순열 구하기

--- PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 6/6. 중복순열 구하기/AA.py
+++ PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 6/6. 중복순열 구하기/AA.py
@@ -1,11 +1,19 @@
 ##########################
-# 시작시간     마무리시간
+# 시작시간  231223 1727  마무리시간 1824
+# '==' 을 써야 될 곳에 '>'을 써서 30분 날림 원래는 1754에 풀음
 
+def DFS(level):
+  if level == m:
+    for i in range(m):
+      print(result[i], end=' ')
+    print()
+  else:
+    for i in range(n):
+      result[level]= i+1
+      DFS(level + 1)
 
-
-
-
-
-
+n, m = map(int, input().split())
+result = [0] * (n + 1)
+DFS(0)
 
 #########################
