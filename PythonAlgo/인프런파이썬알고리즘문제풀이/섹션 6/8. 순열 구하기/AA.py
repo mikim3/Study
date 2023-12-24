6fcd30a1dba93bc84c742d72397ae11baed20244
rlaalstn0107 <minsu980107@naver.com>
2023-12-24 16:29:49 +0900

    Feat(DFS): 순열구하기

--- PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 6/8. 순열 구하기/AA.py
+++ PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 6/8. 순열 구하기/AA.py
@@ -1,11 +1,32 @@
 ##########################
-# 시작시간     마무리시간
+# 시작시간 231224 15:55    마무리시간 1627
+# 전에 풀었던 중복순열 구하기를 봐서 풀수 있었음 다시풀기
 
+# 
+def DFS(level):
+  global count
+  if level == m:
+    result_sorted = sorted(result)
+    # print("result == ",result)
+    # print("result_sorted == ",result_sorted)
+    for i in range(n):
+      if result_sorted[i] != 0:
+        if result_sorted[i] == result_sorted[i+1]:
+          return
+    for i in range(m):
+      print(result[i], end=' ')
+    print()
+    count+=1
+  else:
+    for i in range(1,n+1):
+      result[level] = i
+      DFS(level + 1)
 
+count = 0
+n, m = map(int, input().split())
+result = [0] * (n+1)
 
-
-
-
-
+DFS(0)
+print(count)
 
 #########################
