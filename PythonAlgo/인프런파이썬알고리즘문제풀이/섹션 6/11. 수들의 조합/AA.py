2de529ed1661c2e300936afee37d7644848d9226
mikim3 <minsu980107@naver.com>
2023-12-27 21:49:04 +0900

    Feat(DFS): 수들의 조합

--- PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 6/11. 수들의 조합/AA.py
+++ PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 6/11. 수들의 조합/AA.py
@@ -1,10 +1,36 @@
-import itertools as it
-#sys.stdin=open("input.txt", "r")
-n, k=map(int, input().split())
-a=list(map(int, input().split()))
-m=int(input())
-cnt=0
-for x in it.combinations(a, k):
-    if sum(x)%m==0:
-        cnt+=1
-print(cnt)
+################################
+# 시작시간 231227 21:27 마무리시간 21:45
+# 배수찾기
+
+def DFS(level, start):
+  global count
+  if level == k:
+    su = 0
+    for i in range(k):
+      su+=result[i]
+    print('result ==',result, 'su == ', su)
+    if su % m == 0:
+      count += 1
+  else:
+    for i in range(start, n):
+      result[level] = li[i]
+      DFS(level + 1, i + 1)
+n, k = map(int,input().split())
+li = list(map(int, input().split()))
+m = int(input())
+result = [0] * k
+count = 0
+DFS(0,0)
+print(count)
+
+####################
+# import itertools as it
+# #sys.stdin=open("input.txt", "r")
+# n, k=map(int, input().split())
+# a=list(map(int, input().split()))
+# m=int(input())
+# cnt=0
+# for x in it.combinations(a, k):
+#     if sum(x)%m==0:
+#         cnt+=1
+# print(cnt)
