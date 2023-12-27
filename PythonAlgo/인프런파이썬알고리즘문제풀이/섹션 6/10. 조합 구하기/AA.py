c444c02aa84f829261dfee59691440893c642dd6
mikim3 <minsu980107@naver.com>
2023-12-27 21:48:38 +0900

    Feat(DFS): 조합구하기 복습

--- PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 6/10. 조합 구하기/AA.py
+++ PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 6/10. 조합 구하기/AA.py
@@ -1,27 +1,49 @@
-##########################
-# 시작시간 231226 21:16 마무리시간 22:00
-# 못 풀고 강의에서 아이디어만 듣고 풀기시작
-
+#########################
+# 시작시간 231227 2040 마무리시간 2054
+# 아직 많이 헷갈림
 def DFS(level, start):
   global count
   if level == m:
     for i in range(m):
       print(result[i], end=' ')
     print()
-    count += 1
+    count+=1
   else:
-    # i가 뜻하는건 현재 level에 넣을 result값임
-    for i in range(start - 1, n):
-      if check[i] == 0:
-        check[i] = 1
-        result[level] = i+1
-        DFS(level + 1, i + 1)
-        check[i] = 0
+    # start ~ n 까지 반복
+    for i in range(start, n + 1):
+      result[level] = i
+      # 한 레벨 올라가면 start 값도 1 올라감
+      DFS(level + 1, i + 1)
 n, m = map(int, input().split())
-check = [0] * n
-result = [0] * m
+result = [0] * n
 count = 0
 DFS(0,1)
 print(count)
 
-#########################
+# ##########################
+# # 시작시간 231226 21:16 마무리시간 22:00
+# # 못 풀고 강의에서 아이디어만 듣고 풀기시작
+
+# def DFS(level, start):
+#   global count
+#   if level == m:
+#     for i in range(m):
+#       print(result[i], end=' ')
+#     print()
+#     count += 1
+#   else:
+#     # i가 뜻하는건 현재 level에 넣을 result값임
+#     for i in range(start - 1, n):
+#       if check[i] == 0:
+#         check[i] = 1
+#         result[level] = i+1
+#         DFS(level + 1, i + 1)
+#         check[i] = 0
+# n, m = map(int, input().split())
+# check = [0] * n
+# result = [0] * m
+# count = 0
+# DFS(0,1)
+# print(count)
+
+# #########################
