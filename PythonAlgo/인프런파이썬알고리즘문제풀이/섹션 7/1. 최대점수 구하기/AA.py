8924aa085158e3b572c33fd8841fdfb5ffee0514
rlaalstn0107 <minsu980107@naver.com>
2023-12-30 00:11:37 +0900

    Feat(DFS): 최대점수 구하기

--- PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 7/1. 최대점수 구하기/AA.py
+++ PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 7/1. 최대점수 구하기/AA.py
@@ -1,11 +1,35 @@
 ##########################
-# 시작시간     마무리시간
+# 시작시간 231229 2357    마무리시간
 
+# 순열
+# 
 
+# 점수 푸는시간 순
+# 5 20
+# 10 5
+# 25 12
+# 15 8
+# 6 3
+# 7 4
 
+def DFS(level):
+  if level == n:
+  
+  else:
+    for i in range(n):
+          
 
+n, m = map(int, input().split())
+li= []
+for i in range(n):
+  li.append(list(map(int, input().split())))
+print(li)
+check= [0] * (n+1)
+result = [0] * (n+1)
+# 현재 지난 시간
+now_time = 0
+# 
 
-
-
+max_value = 0
 
 #########################
