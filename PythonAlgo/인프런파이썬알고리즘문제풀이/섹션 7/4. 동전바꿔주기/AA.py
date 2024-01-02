70c4a36918a7227df9c574944d32d6b351f9d1f7
mikim3 <minsu980107@naver.com>
2024-01-02 15:27:56 +0900

    Feat(DFS) : 동전바꿔주기 답봄

--- PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 7/4. 동전바꿔주기/AA.py
+++ PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 7/4. 동전바꿔주기/AA.py
@@ -1,28 +1,54 @@
-##########################
-# 시작시간  19:33   마무리시간
+import sys
+input = sys.stdin.readline
+# #############################
+# # 모범답안
 
-# 2중 for 문으로 각 동전별 갯수 표현
+# def DFS(level, sum):
+#   global count
+#   if sum > T:
+#     return
+#   if level == k:
+#     if sum == T:
+#       count += 1
+#   else:
+#     for i in range(coin_count[level] + 1):
+#       DFS(level+1, sum + (coin_value[level] * i))
+
+# T=int(input())
+# k=int(input())
+# count = 0
+# coin_value = []
+# coin_count = []
+# for i in range(k):
+#   a, b = map(int, input().split())
+#   coin_value.append(a)
+#   coin_count.append(b)
+# DFS(0,0)
+# print(count)
+
+##########################
+# 시작시간 240101 19:33 240102 40분소비 14:46 마무리시간
+# 답봄
+
 def DFS(level):
   global count
   if level == k:
     tmp_sum = 0
     for i in range(k):
-      tmp_sum += li[i][0] * check[i]
+      tmp_sum += li[i][0] * result[i]
     if tmp_sum == t:
       count += 1
   else:
-    for i in range(k):
-      # 각 동전의 갯수만큼 돌기
-      for j in range(li[i][1]):
-        check[i] += 1
-        DFS(level + 1)
-        check[i] -= 1
+    for i in range(li[level][1] + 1):
+      result[level] = i
+      DFS(level + 1)
 
 t= int(input())
+# 가지수
 k = int(input())
 li = []
 # 몇 번째 동전이 몇개 인지
-check = [0] * k
+result = [0] * k
 count = 0
 for i in range(k):
   li.append(list(map(int,input().split())))
