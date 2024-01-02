41de858e9baf014282c0325e49b0a77b08a52555
mikim3 <minsu980107@naver.com>
2024-01-02 15:28:32 +0900

    Feat(DFS): 동전바꿔주기

--- PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 7/4. 동전바꿔주기/AA.py
+++ PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 7/4. 동전바꿔주기/AA.py
@@ -26,33 +26,34 @@ input = sys.stdin.readline
 # DFS(0,0)
 # print(count)
 
-##########################
-# 시작시간 240101 19:33 240102 40분소비 14:46 마무리시간
-# 답봄
+# ##########################
+# # 시작시간 240101 19:33 240102 40분소비 14:46 마무리시간
+# # 답봄
+# # 시간초과해서 안됨
 
-def DFS(level):
-  global count
-  if level == k:
-    tmp_sum = 0
-    for i in range(k):
-      tmp_sum += li[i][0] * result[i]
-    if tmp_sum == t:
-      count += 1
-  else:
-    for i in range(li[level][1] + 1):
-      result[level] = i
-      DFS(level + 1)
+# def DFS(level):
+#   global count
+#   if level == k:
+#     tmp_sum = 0
+#     for i in range(k):
+#       tmp_sum += li[i][0] * result[i]
+#     if tmp_sum == t:
+#       count += 1
+#   else:
+#     for i in range(li[level][1] + 1):
+#       result[level] = i
+#       DFS(level + 1)
 
-t= int(input())
-# 가지수
-k = int(input())
-li = []
-# 몇 번째 동전이 몇개 인지
-result = [0] * k
-count = 0
-for i in range(k):
-  li.append(list(map(int,input().split())))
-DFS(0)
-print(count)
+# t= int(input())
+# # 가지수
+# k = int(input())
+# li = []
+# # 몇 번째 동전이 몇개 인지
+# result = [0] * k
+# count = 0
+# for i in range(k):
+#   li.append(list(map(int,input().split())))
+# DFS(0)
+# print(count)
 
-#########################
+# #########################
