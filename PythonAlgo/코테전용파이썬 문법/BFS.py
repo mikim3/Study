f7d18a438cfd77960e667197ee13b4ceab039490
mikim3 <minsu980107@naver.com>
2024-01-04 17:58:16 +0900

    Feat(DFS,BFS): 기본템플릿코드

---
+++ PythonAlgo/코테전용파이썬 문법/BFS.py
@@ -0,0 +1,34 @@
+from collections import deque
+
+# 가까운 노드 먼저 접근
+
+# 노드A 접근
+def bfs(graph, start_vector):
+  visited = [start_vector]
+  # 사전세팅으로 시작노드를 queue에 넣음
+  queue = deque(start_vector)
+  # while문을 돌면서 queue를 하나씩 돌꺼임
+  while queue:
+    # first in first out
+    cur_v = queue.popleft()
+    # cur_v에 근접한 모든 노드에 접근
+    for v in graph[cur_v]:
+      # 만약 방문 안 했으면 처리
+      if v not in visited:
+        # 실제 방문하고
+        visited.append(v)
+        # 이후를 위해 큐에 쌓아 놓기
+        queue.append(v)
+      print(visited)
+  return visited
+
+graph = {
+  'A': ['B', 'D', 'E'],
+  'B': ['A','C','D'],
+  'C': ['B'],
+  'D': ['A', 'B'],
+  'E': ['A']
+}
+
+bfs(graph, 'A')
+# print(bfs(graph, 'A'))
--- PythonAlgo/코테전용파이썬 문법/DFS.py
+++ PythonAlgo/코테전용파이썬 문법/DFS.py
@@ -1,3 +1,10 @@
+
+def dfs(cur_v):
+  visited.append(cur_v)
+  for v in graph[cur_v]:
+    if v not in visited:
+      dfs(v)
+
 graph = {
   'A': ['B', 'D', 'E'],
   'B': ['A','C','D'],
@@ -5,3 +12,6 @@ graph = {
   'D': ['A', 'B'],
   'E': ['A']
 }
+visited = []
+
+
