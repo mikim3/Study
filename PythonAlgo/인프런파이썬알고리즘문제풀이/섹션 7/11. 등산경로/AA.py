d31b014ebb686515dad6b5693aac4650abddb222
mikim3 <minsu980107@naver.com>
2024-01-12 11:00:20 +0900

    Feat(BFS): 등산경로  --wip--

--- PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 7/11. 등산경로/AA.py
+++ PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 7/11. 등산경로/AA.py
@@ -1,6 +1,18 @@
 ##########################
-# 시작시간     마무리시간
+# 시작시간 240112  02:17   마무리시간
 
+# 다음 경로는 현재 값보다 높아야만 한다.
+
+dx = [-1,0,1,0]
+dy = [0,1,0,-1]
+
+n = int(input())
+li = []
+for i in range(n):
+  li.append(list(map(int, input().split())))
+print(li)
+
+def dfs()
 
 
 
