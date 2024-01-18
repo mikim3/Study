18bd648dab4e82bdea6e0b204d40e4e0ba26cde9
rlaalstn0107 <minsu980107@naver.com>
2024-01-18 23:13:50 +0900

    Feat(DP): UniquePaths

--- PythonAlgo/인프런ALLINONE/DP/UniquePaths.py
+++ PythonAlgo/인프런ALLINONE/DP/UniquePaths.py
@@ -3,13 +3,13 @@
 # m,n 까지 가는 경우의수
 # f(m,n) ==  f(m - 1, n) + f(m, n - 1)
 
-memo = {(0,0):0, (1,0) : 1, (0, 1) : 1}
+memo = {(1,1):1, (2,1) : 1, (1, 2) : 1}
 class Solution:
   def uniquePaths(self, m: int, n: int) -> int:
-    if m < 0 or n < 0:
+    if m < 1 or n < 1:
       return 0
-    if m == 0 and n == 0:
-      return 0
+    if m == 1 and n == 1:
+      return 1
     if m == 1 and n == 0:
       return 1
     if m == 0 and n == 1:
