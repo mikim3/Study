07aa044a0a7cf621273c96ebe0699139e41b73dd
mikim3 <minsu980107@naver.com>
2024-01-23 15:34:00 +0900

    Feat: Counter 문법 작성

---
+++ PythonAlgo/코테전용파이썬 문법/Counter_리스트안에원하는값횟수세기.py
@@ -0,0 +1,10 @@
+from collections import Counter
+
+# 예시 리스트
+some_list = ['apple', 'banana', 'apple', 'orange', 'banana', 'apple']
+
+counter = Counter(some_list)
+
+print(counter)
+print(counter['apple'])
+print(counter['appleegg'])
