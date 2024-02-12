044b3b99262ee52b85bb20851930d38594e0f2e8
rlaalstn0107 <minsu980107@naver.com>
2024-02-13 00:35:37 +0900

    Feat(DFS) : 최대점수구하기 --wip--

--- PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 7/1. 최대점수 구하기/AA.py
+++ PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 7/1. 최대점수 구하기/AA.py
@@ -1,75 +1,116 @@
-###############################
-# 시작시간 20:58 마무리시간 21:22
+##############################
+# 시작시간 240212 10:25 마무리시간
 
-#
-# 시간도 넘겨줘서 조기 종료 해보기
-def dfs(level, nowTime):
-  global high_score
-  if nowTime > m:
-    return
-  if level==n:
-    now_score = 0
+# 현재 시간 인자에 넣기
+
+def dfs(level):
+  global all_time
+  global max_value
+  # if all_time > m:
+  #   return
+  if level >= n:
+    if all_time > m:
+      return
+    tmp = 0
     for i in range(n):
-      if result[i] == 1:
-        now_score += li[i][0]
-    if now_score >= high_score:
-      high_score = now_score
+      if checked[i] == 1:
+        tmp+= li_input[i][0]
+    if tmp >= max_value:
+      max_value = tmp
   else:
-    result[level] = 1
-    dfs(level+1,nowTime+li[level][1])
-    result[level] = 0
-    dfs(level+1, nowTime)
+    checked[level] = 1
+    all_time += li_input[level][1]
+    dfs(level+1)
+    checked[level] = 0
+    all_time -= li_input[level][1]
+      
+      
+# 점수, 걸리는 시간
+n, m = map(int ,input().split())
 
-n, m = map(int, input().split())
-li = []
-# 점수 시간 순 입력
+li_input = []
 for i in range(n):
-  li.append(list(map(int, input().split())))
-print(li)
-count_time = 0
-high_score = 0
-result = [0] * (n)
+  li_input.append(tuple(map(int, input().split())))
+# all_score = [0] * (n+1)
+all_time = 0
+checked = [0] * (n+1)
+max_value = 0
+print(li_input)
+dfs(0)
+print(max_value)
 
-dfs(0, 0)
-print(high_score)
+# ###############################
+# # 시작시간 20:58 마무리시간 21:22
 
-
-
-
-# ##########################
-# # 시작시간 240101 00:05  마무리시간 00:48
-
-# def DFS(level):
-#   global max_value
-#   global now_time
-#   if now_time > m:
+# #
+# # 시간도 넘겨줘서 조기 종료 해보기
+# def dfs(level, nowTime):
+#   global high_score
+#   if nowTime > m:
 #     return
-#   if level == n:
-#     tmp_result = 0
+#   if level==n:
+#     now_score = 0
 #     for i in range(n):
-#       if check[i] == 1:
-#         tmp_result+=li[i][0]
-#     if max_value < tmp_result:
-#       max_value = tmp_result
+#       if result[i] == 1:
+#         now_score += li[i][0]
+#     if now_score >= high_score:
+#       high_score = now_score
 #   else:
-#     check[level] = 1
-#     now_time+= li[level][1]
-#     DFS(level+1)
-#     now_time-= li[level][1]
-#     check[level] = 0
-#     DFS(level+1)
+#     result[level] = 1
+#     dfs(level+1,nowTime+li[level][1])
+#     result[level] = 0
+#     dfs(level+1, nowTime)
 
 # n, m = map(int, input().split())
-# li= []
+# li = []
+# # 점수 시간 순 입력
 # for i in range(n):
 #   li.append(list(map(int, input().split())))
-# # print(li)
-# # 해당 문제 풀었는지 저장
-# check= [0] * (n+1)
-# # result = [0] * (n+1)
-# # 현재 지난 시간
-# now_time = 0
-# max_value = 0
-# DFS(0)
-# print(max_value)
-# #########################
+# print(li)
+# count_time = 0
+# high_score = 0
+# result = [0] * (n)
+
+# dfs(0, 0)
+# print(high_score)
+
+
+
+
+# # ##########################
+# # # 시작시간 240101 00:05  마무리시간 00:48
+
+# # def DFS(level):
+# #   global max_value
+# #   global now_time
+# #   if now_time > m:
+# #     return
+# #   if level == n:
+# #     tmp_result = 0
+# #     for i in range(n):
+# #       if check[i] == 1:
+# #         tmp_result+=li[i][0]
+# #     if max_value < tmp_result:
+# #       max_value = tmp_result
+# #   else:
+# #     check[level] = 1
+# #     now_time+= li[level][1]
+# #     DFS(level+1)
+# #     now_time-= li[level][1]
+# #     check[level] = 0
+# #     DFS(level+1)
+
+# # n, m = map(int, input().split())
+# # li= []
+# # for i in range(n):
+# #   li.append(list(map(int, input().split())))
+# # # print(li)
+# # # 해당 문제 풀었는지 저장
+# # check= [0] * (n+1)
+# # # result = [0] * (n+1)
+# # # 현재 지난 시간
+# # now_time = 0
+# # max_value = 0
+# # DFS(0)
+# # print(max_value)
+# # #########################
