7de3ae2664a009f4abfcbe6c902ebf8101282bde
rlaalstn0107 <minsu980107@naver.com>
2024-02-12 19:14:02 +0900

    Feat(DFS): 수들의 조합

--- PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 6/11. 수들의 조합/AA.py
+++ PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 6/11. 수들의 조합/AA.py
@@ -1,26 +1,52 @@
+##########################
+# 시작시간 240212 1708 마무리시간 1723
+# 오타때문에 10분날림
+
+# def dfs(level, start):
+#   global count
+#   if level == k:
+#     tmp = 0
+#     for i in range(k):
+#       tmp += result[i]
+#     if tmp % m == 0:
+#       count += 1
+#   else:
+#     # i는 지금 몇번째 카드 뽑을지
+#     for i in range(start, n+1):
+#       result[level] = li[i - 1]
+#       dfs(level + 1, i + 1)
+
+# n, k = map(int, input().split())
+# li = list(map(int, input().split()))
+# result = [0] * (k + 1)
+# m = int(input())
+# count = 0
+# dfs(0,1)
+# print(count)
+
 #############################
 # 시작시간 231228 17:13 마무리시간 17:25
 
-def DFS(level,start):
-  global count
-  if level == k:
-    su = 0
-    for i in range(k):
-      su += result[i]
-    if su % m == 0:
-      count += 1
-  else:
-    for i in range(start, n):
-      result[level] = li[i]
-      DFS(level + 1, i + 1)
+# def DFS(level,start):
+#   global count
+#   if level == k:
+#     su = 0
+#     for i in range(k):
+#       su += result[i]
+#     if su % m == 0:
+#       count += 1
+#   else:
+#     for i in range(start, n):
+#       result[level] = li[i]
+#       DFS(level + 1, i + 1)
 
-n, k = map(int,input().split()) # n개 중에 ,k개를 선택
-li = list(map(int,input().split())) # 조합 후보군
-m = int(input())
-result = [0] * k
-count = 0
-DFS(0, 0)
-print(count)
+# n, k = map(int,input().split()) # n개 중에 ,k개를 선택
+# li = list(map(int,input().split())) # 조합 후보군
+# m = int(input())
+# result = [0] * k
+# count = 0
+# DFS(0, 0)
+# print(count)
 
 # ################################
 # # 시작시간 231227 21:27 마무리시간 21:45
