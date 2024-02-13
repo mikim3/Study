e2063d53808ce608fb0345ca1394e01324f145e7
mikim3 <minsu980107@naver.com>
2024-02-13 21:01:57 +0900

    Feat(DFS): 동전분배하기

--- PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 7/5. 동전분배하기/AA.py
+++ PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 7/5. 동전분배하기/AA.py
@@ -1,22 +1,38 @@
 #########################
-# 시작시간 240213 19:00   마무리시간
-
+# 시작시간 240213 20:39   마무리시간 20:50
+# 비효율적인가 고민 했는데 그게 맞음
 def dfs(level):
+  global min_value
   if level == n:
-
+    a,b,c = 0,0,0
+    for i in range(n):
+      if checked[i] == 0:
+        a+=li[i]
+      if checked[i] == 1:
+        b+=li[i]
+      if checked[i] == 2:
+        c+=li[i]
+    if a!=b and b!=c and a!=c:
+      tmp = max(abs(a-b), abs(b-c), abs(a-c))
+      if tmp < min_value:
+        min_value = tmp
   else:
-
+    checked[level] = 0
+    dfs(level+1)
+    checked[level] = 1
+    dfs(level+1)
+    checked[level] = 2
+    dfs(level+1)
 
 # 3명에게 나누어서 총액의 최대 차가 가장 적도록하기
-
 n = int(input())
 li = []
 for i in range(n):
   li.append(int(input()))
 checked = [0] * (n+1)
-
-
-
+min_value = 999999
+dfs(0)
+print(min_value)
 
 # ##########################
 # # 시작시간  240102 15:50   마무리시간 16:40
