be807fcd16cfdbb6762017b99501e1a7c5d67957
rlaalstn0107 <minsu980107@naver.com>
2024-02-21 23:46:51 +0900

    Feat(이분탐색): 랜선자르기

--- PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 4/2. 랜선자르기/AA.py
+++ PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 4/2. 랜선자르기/AA.py
@@ -1,39 +1,65 @@
-# 시작시간 :  11 40   종료시간 :  12:20  
+####################################
+# 시작시간 240221 2309 마무리시간 2335
 
-# 갯수 조건에 맞는 경우를 찾는다.   크기를 키우면서 확인한다.
-
-#
-def Count(len):
-    cnt = 0
-    for x in li:
-        cnt += (x//len)
-    return cnt
-
-k , n = map(int, input().split())
-
+k, n = map(int ,input().split())
 li = []
 for i in range(k):
-    li.append(int(input()))
+  li.append(int(input()))
 
-max_value = max(li)
-
-lt = 1
-rt = max_value 
-# 랜선 갯수
-div = 0
-# 이분탐색의 중간값이자 랜선 갯수를 알아내는 수
+lt = 0
+rt = max(li)
 
 while lt <= rt:
-    mid = (lt + rt) // 2
-    # 랜선 갯수가 n보다 크면  너무 작게 잘랐으니 lt를 옮겨서 mid도 키워서 자르는 랜선의 크기를 높인다.  
-    if Count(mid)>=n:        
-        res = mid
-        lt = mid + 1  #
-    # 너무 크게 잘랐으니 더 작게 자른다.
-    else:
-        rt = mid - 1
-
+  # mid로 잘라보기
+  mid = (lt + rt) // 2
+  total = 0
+  # mid 크기로 잘라보기
+  for i in range(k):
+    total += li[i] // mid
+  # 잘게 나누어짐
+  if total >= n:
+    res = mid
+    lt = mid + 1
+  elif total < n:
+    rt = mid - 1
 print(res)
+
+
+################################
+# 시작시간 :  11 40   종료시간 :  12:20  
+
+# # 갯수 조건에 맞는 경우를 찾는다.   크기를 키우면서 확인한다.
+# def Count(len):
+#     cnt = 0
+#     for x in li:
+#         cnt += (x//len)
+#     return cnt
+
+# k , n = map(int, input().split())
+
+# li = []
+# for i in range(k):
+#     li.append(int(input()))
+
+# max_value = max(li)
+
+# lt = 1
+# rt = max_value 
+# # 랜선 갯수
+# div = 0
+# # 이분탐색의 중간값이자 랜선 갯수를 알아내는 수
+
+# while lt <= rt:
+#     mid = (lt + rt) // 2
+#     # 랜선 갯수가 n보다 크면  너무 작게 잘랐으니 lt를 옮겨서 mid도 키워서 자르는 랜선의 크기를 높인다.  
+#     if Count(mid)>=n:        
+#         res = mid
+#         lt = mid + 1  #
+#     # 너무 크게 잘랐으니 더 작게 자른다.
+#     else:
+#         rt = mid - 1
+
+# print(res)
 
 
 
--- PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 4/3. 뮤직비디오/AA.py
+++ PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 4/3. 뮤직비디오/AA.py
@@ -1,34 +1,33 @@
 # 시작시간 :  11:35   종료시간 : 12:40 
+# n, m = map(int, input().split())
 
-n, m = map(int, input().split())
+# li = list(map(int,input().split()))
 
-li = list(map(int,input().split()))
-
-def Count(capacity):
-    cnt = 1
-    su = 0
+# def Count(capacity):
+#     cnt = 1
+#     su = 0
     
-    for x in li:
-        # mid 값 보다 크면 한 묶음 처리   
-        if su + x > capacity:
-            cnt += 1
-            # 다음 묶음에도 값 더해지게 처리
-            su = x
-        else:
-            su += x
-    return cnt
+#     for x in li:
+#         # mid 값 보다 크면 한 묶음 처리   
+#         if su + x > capacity:
+#             cnt += 1
+#             # 다음 묶음에도 값 더해지게 처리
+#             su = x
+#         else:
+#             su += x
+#     return cnt
 
-sum_li = sum(li)
-lt = 1
-rt = sum_li
-res = 0
+# sum_li = sum(li)
+# lt = 1
+# rt = sum_li
+# res = 0
 
-while lt <= rt:
-    mid = (lt + rt) // 2
-    # 더 크게 묶던가 작게 묶었다면 res에 들어갈 수 있다.
-    if Count(mid) <= m:
-        res = mid
-        rt = mid - 1
-    else:
-        lt = mid + 1
-print(res)
+# while lt <= rt:
+#     mid = (lt + rt) // 2
+#     # 더 크게 묶던가 작게 묶었다면 res에 들어갈 수 있다.
+#     if Count(mid) <= m:
+#         res = mid
+#         rt = mid - 1
+#     else:
+#         lt = mid + 1
+# print(res)
