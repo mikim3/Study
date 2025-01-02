b39f5c4e791f05d680232b2fc989763e70f6915f
rlaalstn0107 <minsu980107@naver.com>
2025-01-03 00:27:51 +0900

    feat(투포인터): 7795_먹을 것인가 먹힐것인가

--- PythonAlgo/소마_대비스터디_with_현수님/BFS/7795_먹을_것인가_먹힐_것인가.py
+++ PythonAlgo/소마_대비스터디_with_현수님/BFS/7795_먹을_것인가_먹힐_것인가.py
@@ -1,14 +1,24 @@
-# 시작시간 1743
+# 시작시간 1743  마무리시간 18
+# 시간 초과 걸림
+# 투 포인터가 왜 투 포인터인지 생각해보면 이해하기 쉬움
+
+# 쓸데없이 탐색하는 경우
+# 
 t = int(input())
 
-for i in range(t):
+for _ in range(t):
   n, m= map(int, input().split())
   li_a = list(map(int, input().split()))
   li_b = list(map(int, input().split()))
+
+  li_a.sort()
+  li_b.sort()
+
   count = 0
-  # a보다 B가 작은쌍
-  for j  in range(n):
-    for z in range(m):
-      if li_a[j] > li_b[z]:
-        count+=1
+  b_p = 0
+  for a_p in range(n):
+    while b_p < m and li_a[a_p] > li_b[b_p]:
+      b_p += 1
+    count += b_p
+
   print(count)
