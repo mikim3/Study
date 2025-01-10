7360f9c64db5f980996088182ccaed28572518c5
minsukim <minsu980107@naver.com>
2025-01-10 21:38:15 +0900

    feat(bfs): 16234_인구이동

--- PythonAlgo/소마_대비스터디_with_현수님/BFS/16234_인구이동.py
+++ PythonAlgo/소마_대비스터디_with_현수님/BFS/16234_인구이동.py
@@ -1,5 +1,4 @@
-# 시작시간 2000 마무리시간
-
+# 시작시간 2000 마무리시간 2137
 from collections import deque
 import math
 # L명 이상 R명이하 인구 차 발생시 하루 문염
@@ -9,27 +8,36 @@ import math
 
 # 연합의 국가 갯수(땅사이즈)랑 연합인원 리턴하는게 좋을듯
 def bfs(x, y, graph):
-  #
-  hap = 0 # 현재연합의 합
-  #
+  hap = graph[x][y] # 현재연합의 합
+  size = 1
+  checked[x][y] = 1
   queue =deque()
   queue.append((x,y, graph[x][y]))
+  # 현재 연합들의 좌표
+  li_now_yun = [(x, y)]
   while queue:
     now = queue.popleft()
     for i in range(4):
-      nx = now[0] + dx
-      ny = now[1] + dy
-      # 현재 좌표 인구 뺴기 다음 좌표 인구
-      cha = abs(now[2] - graph[nx][ny])
+      nx = now[0] + dx[i]
+      ny = now[1] + dy[i]
       if nx < 0 or nx >= n or ny < 0 or ny >= n:
         continue
+      # 현재 좌표 인구 뺴기 다음 좌표 인구
+      cha = abs(now[2] - graph[nx][ny])
       # checked == 0 을 넣어야 되나???? 고민되네
-      if l <= cha <= r:
-        queue.append((nx,ny,graph[nx][ny]))
+      if l <= cha <= r and checked[nx][ny] == 0:
+        queue.append((nx, ny, graph[nx][ny]))
         checked[nx][ny] = 1
-
-
-
+        hap += graph[nx][ny]
+        size += 1
+        li_now_yun.append((nx, ny))
+  if size > 1:
+    # 연합 인원 결정
+    yun_value = hap // size
+    for i in range(len(li_now_yun)):
+      now_x, now_y= li_now_yun[i]
+      graph[now_x][now_y] = yun_value
+  return size # size가 1이면 연합을 형성 못 한 것이다.
 # N, L, R
 n, l, r = map(int,input().split())
 dx = [-1,0,1,0]
@@ -53,13 +61,13 @@ while True:
   for i in range(n):
     for j in range(n):
       # 이미 연합으로 계산 된 곳은 계산 X
-      if checked != 1:
-        bfs(i,j, graph)
-        yun_number += 1
+      if checked[i][j] != 1:
+        # 1이면 연합 못 이룬거임
+        if bfs(i,j, graph) != 1:
+          yun_number += 1
   # 다 돌았으므로 하루 지남
   day += 1
   # 연합이 하나도 형성 안되면 그 날이 마지막 날
   if yun_number == 0:
     break
-
-
+print(day - 1)
