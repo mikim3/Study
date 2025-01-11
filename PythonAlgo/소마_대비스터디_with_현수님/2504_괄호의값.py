f6b1ae8b1bd1587260288a432a700c3003b1b759
rlaalstn0107 <minsu980107@naver.com>
2025-01-12 00:43:27 +0900

    wip 2504

--- PythonAlgo/소마_대비스터디_with_현수님/2504_괄호의값.py
+++ PythonAlgo/소마_대비스터디_with_현수님/2504_괄호의값.py
@@ -1,9 +1,7 @@
 # 시작시간 2330
+# 0040에 답봄    
 
-#
-# 
 # 숫자 있으면 괄호값 * 숫자값
-# 
 
 # 옆에 나열돼 있으면 +
 # 안에 들어가 있으면 곱하기계산해야함
@@ -23,28 +21,22 @@ for i in range(n):
     stack.append('[')
   if in_value[i] == ')':
     if stack and stack[-1] == '(':
-      stack.pop()
-      ans += 2
-      if stack:
-        for j in range(len(stack)):
-          if stack[j] == '(':
-            ans *= 2
-          if stack[j] == '[':
-            ans *= 3
+      if in_value[i-1] == '(':
+        ans += 2
+      else:
+        ans *= 2
     else:
       flag = 1
+    stack.pop()
   if in_value[i] == ']':
     if stack and stack[-1] == '[':
-      stack.pop()
-      ans += 3
-      if stack:
-        for j in range(len(stack)):
-          if stack[j] == '(':
-            ans *= 2
-          if stack[j] == '[':
-            ans *= 3
+      if in_value[i-1] == '[':
+        ans += 3
+      else:
+        ans *= 3
     else:
       flag = 1
+    stack.pop()
   if flag == 1:
     break
   
