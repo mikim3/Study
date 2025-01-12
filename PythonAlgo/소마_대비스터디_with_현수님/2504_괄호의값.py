9dca1994c6e8a33f511619c328236f0bb16e4652
rlaalstn0107 <minsu980107@naver.com>
2025-01-12 23:48:55 +0900

    feat(스택): 괄호의값

--- PythonAlgo/소마_대비스터디_with_현수님/2504_괄호의값.py
+++ PythonAlgo/소마_대비스터디_with_현수님/2504_괄호의값.py
@@ -8,39 +8,49 @@
 
 # 올바르지 못하면 0 출력
 
-in_value = input()
-n = len(in_value)
-stack = []
-# 0으로 유지돼야 정상
-flag = 0
-ans = 0
-for i in range(n):
-  if in_value[i] == '(':
-    stack.append('(')
-  if in_value[i] == '[':
-    stack.append('[')
-  if in_value[i] == ')':
-    if stack and stack[-1] == '(':
-      if in_value[i-1] == '(':
-        ans += 2
-      else:
-        ans *= 2
+in_value = input()  # 입력값 받기
+stack = []          # 괄호를 저장할 스택
+result = 0          # 최종 결과 값
+temp = 1            # 괄호의 값을 계산하기 위한 임시 변수
+
+for i in range(len(in_value)):  # 문자열의 각 문자 검사
+    char = in_value[i]
+
+    if char == '(':
+        stack.append(char)  # '('를 스택에 추가
+        temp *= 2           # '('는 값이 2로 곱해져야 하므로 temp에 2를 곱함
+    elif char == '[':
+        stack.append(char)  # '['를 스택에 추가
+        temp *= 3           # '['는 값이 3으로 곱해져야 하므로 temp에 3을 곱함
+    elif char == ')':
+        # 닫는 괄호 ')'가 올 때:
+        if not stack or stack[-1] != '(':
+            # 스택이 비어 있거나 짝이 맞지 않으면 올바르지 않은 괄호열
+            result = 0
+            break
+        if in_value[i - 1] == '(':
+            # 직전 문자가 '('라면 temp 값을 result에 더함
+            result += temp
+        stack.pop()  # '('를 스택에서 제거
+        temp //= 2   # '('에 대한 값을 제거하므로 temp에서 2를 나눔
+    elif char == ']':
+        # 닫는 괄호 ']'가 올 때:
+        if not stack or stack[-1] != '[':
+            # 스택이 비어 있거나 짝이 맞지 않으면 올바르지 않은 괄호열
+            result = 0
+            break
+        if in_value[i - 1] == '[':
+            # 직전 문자가 '['라면 temp 값을 result에 더함
+            result += temp
+        stack.pop()  # '['를 스택에서 제거
+        temp //= 3   # '['에 대한 값을 제거하므로 temp에서 3을 나눔
     else:
-      flag = 1
-    stack.pop()
-  if in_value[i] == ']':
-    if stack and stack[-1] == '[':
-      if in_value[i-1] == '[':
-        ans += 3
-      else:
-        ans *= 3
-    else:
-      flag = 1
-    stack.pop()
-  if flag == 1:
-    break
-  
-if flag == 1:
-  print(0)
-else:
-  print(ans)
+        # 잘못된 문자가 들어온 경우
+        result = 0
+        break
+
+# 반복문이 끝난 후 스택이 비어 있지 않으면 올바르지 않은 괄호열
+if stack:
+    result = 0
+
+print(result)  # 결과 출력
