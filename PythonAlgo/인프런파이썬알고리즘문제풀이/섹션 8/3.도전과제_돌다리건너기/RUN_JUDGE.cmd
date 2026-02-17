13d18469e4fb40a8ef755fa0e35bbda24703f05c
mikim3 <sukim0107@gmail.com>
2026-02-17 16:06:18 +0900

    chore: 채점 프로그램 넣기

---
+++ PythonAlgo/인프런파이썬알고리즘문제풀이/섹션 8/3.도전과제_돌다리건너기/RUN_JUDGE.cmd
@@ -0,0 +1,16 @@
+@echo off
+setlocal EnableExtensions
+
+set "HERE=%~dp0"
+for %%I in ("%HERE%..") do set "ROOT=%%~fI"
+
+if not exist "%ROOT%\judge.cmd" (
+  echo [ERROR] Parent judge.cmd not found: %ROOT%\judge.cmd
+  pause
+  exit /b 1
+)
+
+for %%I in ("%HERE%.") do set "PROBLEM=%%~nxI"
+call "%ROOT%\judge.cmd" "%PROBLEM%"
+pause
+
